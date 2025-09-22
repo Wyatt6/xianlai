1b9a16a7deee28b6896811ceeba1ae6ad5c9de57
WyattMac <wyatt6@163.com>
2025-09-22 22:28:24 +0800

    网关实现动态路由功能

---
+++ xianlai-microservice-gateway/src/main/java/fun/xianlai/microservice/gateway/service/NacosConfigService.java
@@ -0,0 +1,55 @@
+package fun.xianlai.microservice.gateway.service;
+
+import com.alibaba.nacos.api.NacosFactory;
+import com.alibaba.nacos.api.config.ConfigService;
+import com.alibaba.nacos.api.exception.NacosException;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+import java.util.Properties;
+
+/**
+ * @author WyattLau
+ */
+@Slf4j
+@Service
+public class NacosConfigService {
+    @Value("${nacos.server-addr}")
+    private String SERVER_ADDR;
+    @Value("${nacos.namespace}")
+    private String NAMESPACE;
+    @Value("${nacos.username}")
+    private String USERNAME;
+    @Value("${nacos.password}")
+    private String PASSWORD;
+
+    public ConfigService initConfigService() {
+        try {
+            log.info("初始化ConfigService...");
+            Properties p = new Properties();
+            if (SERVER_ADDR != null) {
+                p.setProperty("serverAddr", SERVER_ADDR);
+                log.info("serverAddr = {}", SERVER_ADDR);
+            }
+            if (NAMESPACE != null) {
+                p.setProperty("namespace", NAMESPACE);
+                log.info("namespace = {}", NAMESPACE);
+            }
+            if (USERNAME != null) {
+                p.setProperty("username", USERNAME);
+                log.info("username = {}", USERNAME);
+            }
+            if (PASSWORD != null) {
+                p.setProperty("password", PASSWORD);
+                log.info("password = {}", PASSWORD);
+            }
+            log.info("ConfigService已初始化");
+            return NacosFactory.createConfigService(p);
+        } catch (NacosException e) {
+            log.error("初始化ConfigService异常");
+            log.error("{}", e.getMessage());
+            return null;
+        }
+    }
+}
