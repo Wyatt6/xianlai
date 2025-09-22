0a9a6b80d737fd2c8e3bcc0dcff79697f3fbf82a
WyattMac <wyatt6@163.com>
2025-09-22 22:55:56 +0800

    网关日志打印

---
+++ xianlai-microservice-gateway/src/main/java/fun/xianlai/microservice/gateway/logger/RequestLogFilter.java
@@ -0,0 +1,32 @@
+package fun.xianlai.microservice.gateway.logger;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.server.reactive.ServerHttpRequest;
+import org.springframework.stereotype.Component;
+import org.springframework.web.server.ServerWebExchange;
+import org.springframework.web.server.WebFilter;
+import org.springframework.web.server.WebFilterChain;
+import reactor.core.publisher.Mono;
+
+/**
+ * 打印每个请求的分界线
+ *
+ * @author WyattLau
+ */
+@Slf4j
+@Component
+public class RequestLogFilter implements WebFilter {
+    @Override
+    public Mono<Void> filter(ServerWebExchange exchange, WebFilterChain chain) {
+        // 处理请求
+        return chain.filter(exchange)
+                .contextWrite(context -> {
+                    ServerHttpRequest request = exchange.getRequest();
+                    String url = request.getURI().toString();
+                    if (!url.matches(".*/actuator.*")) {
+                        log.info(">>>>> 开始处理请求: {} {}", request.getMethod(), request.getURI());
+                    }
+                    return context;
+                });
+    }
+}
