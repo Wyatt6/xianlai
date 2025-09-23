d3f2e7c2d6d057c6cf5eaa2d830a40fe235295f4
WyattMac <wyatt6@163.com>
2025-09-23 11:51:12 +0800

    配置MySQL数据库连接

---
+++ xianlai-microservice-common/src/main/java/fun/xianlai/microservice/common/properties/MysqlProperties.java
@@ -0,0 +1,20 @@
+package fun.xianlai.microservice.common.properties;
+
+import lombok.Data;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+
+/**
+ * @author WyattLau
+ */
+@Data
+@Component
+@ConfigurationProperties("datasource.mysql")
+public class MysqlProperties {
+    private String host;
+    private String port;
+    private String db;
+    private String username;
+    private String password;
+    private String publicKey;
+}
