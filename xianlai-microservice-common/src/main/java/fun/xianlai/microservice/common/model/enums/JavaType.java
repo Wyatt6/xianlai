e940c7befab44a273f170d5be4a2b59abbd4a9ab
WyattMac <wyatt6@163.com>
2025-09-23 23:28:55 +0800

    系统参数实体类

---
+++ xianlai-microservice-common/src/main/java/fun/xianlai/microservice/common/model/enums/JavaType.java
@@ -0,0 +1,16 @@
+package fun.xianlai.microservice.common.model.enums;
+
+/**
+ * @author WyattLau
+ */
+public enum JavaType {
+    BYTE,
+    SHORT,
+    INT,
+    LONG,
+    FLOAT,
+    DOUBLE,
+    CHAR,
+    BOOLEAN,
+    STRING
+}
---
+++ xianlai-microservice-common/src/main/java/fun/xianlai/microservice/common/model/enums/JsType.java
@@ -0,0 +1,13 @@
+package fun.xianlai.microservice.common.model.enums;
+
+/**
+ * @author WyattLau
+ */
+public enum JsType {
+    NUMBER,
+    STRING,
+    BOOLEAN,
+    BIGINT,
+    OBJECT,
+    ARRAY
+}
