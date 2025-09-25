448a06e4755b53294f3defeaa8702308cfc1149b
WyattMac <wyatt6@163.com>
2025-09-25 10:09:32 +0800

    common v1.2.0 新特性：验证码生成和验证

---
+++ xianlai-microservice-common/src/main/java/fun/xianlai/microservice/common/service/CaptchaService.java
@@ -0,0 +1,25 @@
+package fun.xianlai.microservice.common.service;
+
+
+import fun.xianlai.basic.support.DataMap;
+
+/**
+ * @author WyattLau
+ */
+public interface CaptchaService {
+    /**
+     * 生成验证码
+     *
+     * @return {captchaKey 验证码KEY, captchaImage 验证码Base64图像}
+     */
+    DataMap generateCaptcha();
+
+    /**
+     * 校验验证码
+     * 若校验错误则抛出异常
+     *
+     * @param captchaKey 验证码KEY
+     * @param captcha    输入的验证码
+     */
+    void verifyCaptcha(String captchaKey, String captcha);
+}
