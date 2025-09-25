e761585120fef51e99756a654b735378e3dc7b2a
WyattMac <wyatt6@163.com>
2025-09-25 14:14:39 +0800

    iam v1.0.0 新特性：创建IAM模块

---
+++ xianlai-microservice-iam/src/main/java/fun/xianlai/microservice/iam/Application.java
@@ -0,0 +1,19 @@
+package fun.xianlai.microservice.iam;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+/**
+ * @author WyattLau
+ */
+@Slf4j
+@SpringBootApplication(scanBasePackages = "fun.xianlai")
+@EnableDiscoveryClient
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+        log.info("--------------- 微服务 xianlai-microservice-iam 已启动 ---------------");
+    }
+}
