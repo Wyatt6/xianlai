2cffc9d7a45db906c575d428e7a5a241b0da6a1a
WyattMac <wyatt6@163.com>
2025-09-25 09:24:14 +0800

    扩大bean扫描范围

--- xianlai-microservice-common/src/main/java/fun/xianlai/microservice/common/Application.java
+++ xianlai-microservice-common/src/main/java/fun/xianlai/microservice/common/Application.java
@@ -9,7 +9,7 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
  * @author WyattLau
  */
 @Slf4j
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = "fun.xianlai")
 @EnableDiscoveryClient
 public class Application {
     public static void main(String[] args) {
