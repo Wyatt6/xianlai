3032ab070a14be016b0db3babc66a282eecdb3d7
WyattMac <wyatt6@163.com>
2025-09-26 09:42:26 +0800

    重整项目结构

---
+++ xianlai-app-common/src/main/java/fun/xianlai/microservice/common/controller/feign/FeignOptionController.java
@@ -0,0 +1,26 @@
+package fun.xianlai.microservice.common.controller.feign;
+
+import fun.xianlai.microservice.common.service.OptionService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Optional;
+
+/**
+ * @author WyattLau
+ */
+@RestController
+@RequestMapping("/feign/option")
+public class FeignOptionController {
+    @Autowired
+    private OptionService optionService;
+
+//    @FeignProducerLog("从缓存获取某个加载到后端的系统参数值")
+    @GetMapping("/getCertainBackLoadSysOptionValueFromCache")
+    public Optional<Object> getCertainBackLoadSysOptionValueFromCache(@RequestParam("key") String key) {
+        return optionService.getCertainBackLoadSysOptionValueFromCache(key);
+    }
+}
