1ee62c82de1bbb1907ed97a2a6df398e98d0fe14
WyattMac <wyatt6@163.com>
2025-09-28 14:43:36 +0800

    优化RetResult对象

--- xianlai-basic/src/main/java/fun/xianlai/basic/support/RetResult.java
+++ xianlai-basic/src/main/java/fun/xianlai/basic/support/RetResult.java
@@ -11,7 +11,7 @@ import java.util.Map;
 @Getter
 public class RetResult {
     private Boolean success;                // true - 成功 / false - 失败
-    private DataMap data;                   // 返回数据
+    private DataMap data = new DataMap();   // 返回数据
     private String traceId;                 // 日志跟踪ID
 
     public RetResult() {
@@ -29,27 +29,16 @@ public class RetResult {
     }
 
     public RetResult setFailCode(String code) {
-        if (this.data == null) {
-            this.data = new DataMap("failCode", code);
-        } else {
-            this.data.put("failCode", code);
-        }
+        this.data.put("failCode", code);
         return this;
     }
 
     public RetResult setFailMessage(String message) {
-        if (this.data == null) {
-            this.data = new DataMap("failMessage", message);
-        } else {
-            this.data.put("failMessage", message);
-        }
+        this.data.put("failMessage", message);
         return this;
     }
 
     public RetResult addChecksum(String key, String checksum) {
-        if (this.data == null) {
-            this.data = new DataMap();
-        }
         if (!this.data.containsKey("checksum")) {
             this.data.put("checksum", new HashMap<String, String>());
         }
@@ -63,11 +52,12 @@ public class RetResult {
     }
 
     public RetResult addData(String key, Object value) {
-        if (this.data == null) {
-            this.data = new DataMap(key, value);
-        } else {
-            this.data.put(key, value);
-        }
+        this.data.put(key, value);
+        return this;
+    }
+
+    public RetResult writeFeignData(Object value) {
+        this.success().addData("feignResult", value);
         return this;
     }
 
