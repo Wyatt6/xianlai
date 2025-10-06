1a4128d335b70661272abfe62bf673b6c590a4f4
WyattMac <wyatt6@163.com>
2025-10-06 20:23:04 +0800

    Update data-other.sql

--- xianlai-app-common/src/main/resources/data-other.sql
+++ xianlai-app-common/src/main/resources/data-other.sql
@@ -1,2 +1,3 @@
 INSERT IGNORE INTO tb_common_sys_option(`sort_id`, `back_load`, `front_load`, `js_type`, `option_key`, `option_value`, `default_value`, `name`, `description`) VALUE
-(1, 0, 1, 'BOOLEAN', 'console.openLog', 'false', 'false', '控制台日志打印开关', 'true/false');
+(1, 0, 1, 'BOOLEAN', 'console.openLog', 'false', 'false', '控制台日志打印开关', 'true/false'),
+(1, 0, 1, 'NUMBER', 'request.timeout', '10000', '10000', '请求超时时长', '单位：毫秒');
