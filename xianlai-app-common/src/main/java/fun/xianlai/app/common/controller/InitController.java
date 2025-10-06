7821973e53aa182c4c250117faae910b917ac7a0
WyattMac <wyatt6@163.com>
2025-10-06 19:01:40 +0800

    获取初始化数据API

---
+++ xianlai-app-common/src/main/java/fun/xianlai/app/common/controller/InitController.java
@@ -0,0 +1,27 @@
+package fun.xianlai.app.common.controller;
+
+import fun.xianlai.app.common.service.OptionService;
+import fun.xianlai.basic.annotation.ControllerLog;
+import fun.xianlai.basic.support.RetResult;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author WyattLau
+ */
+@Slf4j
+@RestController
+@RequestMapping("/init")
+public class InitController {
+    @Autowired
+    private OptionService optionService;
+
+    @ControllerLog("获取初始化数据")
+    @GetMapping("/getInitData")
+    public RetResult getInitData() {
+        return new RetResult().success().addData("options", optionService.getFrontLoadSysOptionsFromCache());
+    }
+}
