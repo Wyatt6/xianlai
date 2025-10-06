e579de2b9eae6c2743c1678ae4456cabd8a5b2d6
WyattMac <wyatt6@163.com>
2025-10-06 20:17:13 +0800

    Create data-other.sql

---
+++ xianlai-app-common/src/main/resources/data-other.sql
@@ -0,0 +1,2 @@
+INSERT IGNORE INTO tb_common_sys_option(`sort_id`, `back_load`, `front_load`, `js_type`, `option_key`, `option_value`, `default_value`, `name`, `description`) VALUE
+(1, 0, 1, 'BOOLEAN', 'console.openLog', 'false', 'false', '控制台日志打印开关', 'true/false');
