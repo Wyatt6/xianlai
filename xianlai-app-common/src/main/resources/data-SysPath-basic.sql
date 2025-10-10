67eeaf998ebea5053fb03ae137edd9abf34630ff
WyattMac <wyatt6@163.com>
2025-10-10 17:43:21 +0800

    系统路径初始数据

--- xianlai-app-common/src/main/resources/data-SysPath-basic.sql
+++ xianlai-app-common/src/main/resources/data-SysPath-basic.sql
@@ -2,4 +2,7 @@ INSERT IGNORE INTO tb_common_sys_path(`name`, `path`, `sort_id`) VALUE
 ('PORTAL', '/portal', 1),
 ('LOGIN', '/portal/login', 2),
 ('REGISTER', '/portal/register', 3),
-('RESET_PASSWORD', '/portal/reset-password', 4);
+('RESET_PASSWORD', '/portal/reset-password', 4),
+('INDEX', '/', 5),
+('INDEX_REDIRECT', '/homepage', 6),
+('HOMEPAGE', '/homepage', 7);
---
+++ xianlai-app-common/src/main/resources/data-SysPath-iam.sql
@@ -0,0 +1,6 @@
+INSERT IGNORE INTO tb_common_sys_path(`name`, `path`, `sort_id`) VALUE
+('IAM', '/iam', 1001),
+('IAM_REDIRECT', '/iam/user-manage', 1002),
+('IAM_USER_MANAGE', '/iam/user-manage', 1003),
+('IAM_ROLE_MANAGE', '/iam/role-manage', 1004),
+('IAM_PERMISSION_MANAGE', '/iam/permission-manage', 1005);
