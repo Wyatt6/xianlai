57a5c0ee538fdfcf2e14706dddf162e33b139422
WyattMac <wyatt6@163.com>
2025-10-10 16:22:25 +0800

    系统路径增加排序号

--- xianlai-app-common/src/main/resources/data-SysPath-basic.sql
+++ xianlai-app-common/src/main/resources/data-SysPath-basic.sql
@@ -1,5 +1,5 @@
 INSERT IGNORE INTO tb_common_sys_path(`name`, `path`, `sort_id`) VALUE
-('PORTAL', '/portal', 4),
-('LOGIN', '/portal/login', 3),
-('REGISTER', '/portal/register', 2),
-('RESET_PASSWORD', '/portal/reset-password', 1);
+('PORTAL', '/portal', 1),
+('LOGIN', '/portal/login', 2),
+('REGISTER', '/portal/register', 3),
+('RESET_PASSWORD', '/portal/reset-password', 4);
