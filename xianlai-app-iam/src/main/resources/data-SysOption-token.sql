78d5bf1fe2b685a9f84d34c86855da103cef19b1
WyattMac <wyatt6@163.com>
2025-10-11 21:15:22 +0800

    修改系统参数初始数据

--- xianlai-app-iam/src/main/resources/data-SysOption-token.sql
+++
@@ -1,3 +0,0 @@
-INSERT IGNORE INTO tb_common_sys_option(`sort_id`, `back_load`, `front_load`, `js_type`, `option_key`, `option_value`, `default_value`, `name`, `description`) VALUE
-(1, 1, 0, null, 'token.timeout', '43200', '43200', '登录令牌过期时长', '单位：秒'),
-(1, 1, 0, null, 'token.activeTimeout', '3600', '3600', '登录令牌无操作失效时长', '单位：秒');
--- xianlai-app-iam/src/main/resources/data-SysOption-user.sql
+++
@@ -1,9 +0,0 @@
-INSERT IGNORE INTO tb_common_sys_option(`sort_id`, `back_load`, `front_load`, `js_type`, `option_key`, `option_value`, `default_value`, `name`, `description`) VALUE
-(1, 0, 1, 'NUMBER', 'user.username.minLen', '5', '5', '用户名最小长度', '须符合正则表达式要求'),
-(1, 0, 1, 'NUMBER', 'user.username.maxLen', '20', '20', '用户名最大长度', '须符合正则表达式要求'),
-(1, 1, 1, 'STRING', 'user.username.regexp', '^[a-zA-Z][a-zA-Z_0-9]{4,19}$', '^[a-zA-Z][a-zA-Z_0-9]{4,19}$', '用户名正则表达式', null),
-(1, 0, 1, 'STRING', 'user.username.tips', '仅限大写、小写字母，数字，下划线(_)，必须以字母开头', null, '用户名格式提示', '须符合正则表达式要求'),
-(1, 0, 1, 'NUMBER', 'user.password.minLen', '6', '6', '密码最小长度', '须符合正则表达式要求'),
-(1, 0, 1, 'NUMBER', 'user.password.maxLen', '30', '30', '密码最大长度', '须符合正则表达式要求'),
-(1, 1, 1, 'STRING', 'user.password.regexp', '^[a-zA-Z_0-9.~!@#$%^&*?]{6,30}$', '^[a-zA-Z_0-9.~!@#$%^&*?]{6,30}$', '密码正则表达式', null),
-(1, 0, 1, 'STRING', 'user.password.tips', '仅限大写、小写字母，数字，下划线(_)，特殊字符(.~!@#$%^&*?)', null, '密码格式提示', '须符合正则表达式要求');
