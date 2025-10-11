8bd247020a59a884c2935b0bc1582cc3c5ad604d
WyattMac <wyatt6@163.com>
2025-10-11 15:39:48 +0800

    新增系统接口

--- xianlai-app-iam/src/main/resources/data-SysApi.sql
+++ xianlai-app-iam/src/main/resources/data-SysApi.sql
@@ -1,4 +1,4 @@
 INSERT IGNORE INTO tb_common_sys_api(`id`, `call_path`, `description`, `request_method`, `url`) VALUE
 (200001, 'iam.user.register', '注册新用户', 'POST', '/api/iam/user/register'),
-(200002, 'iam.user.login', '用户登录', 'POST', '/api/iam/user/login');
-
+(200002, 'iam.user.login', '用户登录', 'POST', '/api/iam/user/login'),
+(200003, 'iam.user.logout', '退出登录', 'GET', '/api/iam/user/logout');
