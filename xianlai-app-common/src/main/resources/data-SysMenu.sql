3b493e6ad3330df7eed9aca2619e6f419a55579f
WyattMac <wyatt6@163.com>
2025-10-11 20:54:30 +0800

    Update data-SysMenu.sql

--- xianlai-app-common/src/main/resources/data-SysMenu.sql
+++ xianlai-app-common/src/main/resources/data-SysMenu.sql
@@ -1,2 +1,2 @@
 INSERT IGNORE INTO tb_common_sys_menu(`id`, `sort_id`, `icon`, `title`, `path_name`, `permission`, `active`, `parent_id`) VALUE
-(100001,100001, 'ri-home-4-fill', '首页', 'HOMEPAGE', null, 1, 0);
+(100001, 100001, 'ri-home-4-fill', '首页', 'HOMEPAGE', null, 1, 0);
