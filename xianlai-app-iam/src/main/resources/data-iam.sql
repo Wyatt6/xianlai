fe5a487110b816d185ed49b8ccd350a97b211510
WyattMac <wyatt6@163.com>
2025-10-12 19:54:46 +0800

    添加初始权限数据

--- xianlai-app-iam/src/main/resources/data-iam.sql
+++ xianlai-app-iam/src/main/resources/data-iam.sql
@@ -1,6 +1,18 @@
+-- 系统初始权限 --
+INSERT IGNORE INTO tb_iam_permission(`id`, `identifier`, `name`, `description`, `sort_id`) VALUE
+(200001, 'menu:iam', '“身份认证与访问管理”菜单', null, 200001),
+(200002, 'menu:iam_user_manage', '“用户管理”菜单', null, 200002),
+(200003, 'menu:iam_role_manage', '“角色管理”菜单', null, 200003),
+(200004, 'menu:iam_permission_manage', '“权限管理”菜单', null, 200004);
+
 -- 系统初始角色 --
 INSERT IGNORE INTO tb_iam_role(`id`, `identifier`, `name`, `description`, `active`, `sort_id`) VALUE
 (200001, 'super_admin', '超级管理员', null, 1, 200001);
+INSERT IGNORE INTO tb_iam_role_permission(`role_id`, `permission_id`) VALUE
+(200001, 200001),
+(200001, 200002),
+(200001, 200003),
+(200001, 200004);
 
 -- 系统初始用户 --
 INSERT IGNORE INTO tb_iam_user(`id`, `username`, `password`, `salt`, `register_time`, `active`) VALUE
