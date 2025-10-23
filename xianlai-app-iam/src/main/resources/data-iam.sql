71369c0f0983c656aae50c220af46509632775a1
WyattLau <wyatt6@163.com>
2025-10-23 14:31:40 +0800

    优化

--- xianlai-app-iam/src/main/resources/data-iam.sql
+++ xianlai-app-iam/src/main/resources/data-iam.sql
@@ -1,55 +1,57 @@
--- 系统初始权限 --
-INSERT IGNORE INTO tb_iam_permission(`id`, `identifier`, `name`, `description`, `sort_id`) VALUE
+-- ##### Permission ##### --
+INSERT IGNORE INTO tb_iam_permission(`id`, `sort_id`, `identifier`, `name`, `description`) VALUE
 -- 菜单/路由权限 --
-(200001, 'menu:iam', '“身份认证与访问管理”菜单', null, 200001),
-(200002, 'menu:iam_user_manage', '“用户管理”菜单', null, 200002),
-(200003, 'menu:iam_role_manage', '“角色管理”菜单', null, 200003),
-(200004, 'menu:iam_permission_manage', '“权限管理”菜单', null, 200004),
+(20001, 20001, 'menu:iam', '“身份认证与访问管理”菜单', null),
+(20002, 20002, 'menu:iam_user_manage', '“用户管理”菜单', null),
+(20003, 20003, 'menu:iam_role_manage', '“角色管理”菜单', null),
+(20004, 20004, 'menu:iam_permission_manage', '“权限管理”菜单', null),
 -- 权限 --
-(201001, 'permission:add', '添加权限', null, 201001),
-(201002, 'permission:delete', '删除权限', null, 201002),
-(201003, 'permission:edit', '修改权限', null, 201003),
-(201004, 'permission:query', '查询权限', null, 201004),
+(21001, 21001, 'permission:add', '添加权限', null),
+(21002, 21002, 'permission:delete', '删除权限', null),
+(21003, 21003, 'permission:edit', '修改权限', null),
+(21004, 21004, 'permission:query', '查询权限', null),
 -- 角色 --
-(202001, 'role:add', '添加角色', null, 202001),
-(202002, 'role:delete', '删除角色', null, 202002),
-(202003, 'role:edit', '修改角色', null, 202003),
-(202004, 'role:query', '查询角色', null, 202004),
+(22001, 22001, 'role:add', '添加角色', null),
+(22002, 22002, 'role:delete', '删除角色', null),
+(22003, 22003, 'role:edit', '修改角色', null),
+(22004, 22004, 'role:query', '查询角色', null),
 -- 用户 --
-(203001, 'user:add', '添加用户', null, 203001),
-(203002, 'user:delete', '删除用户', null, 203002),
-(203003, 'user:edit', '修改用户', null, 203003),
-(203004, 'user:query', '查询用户', null, 203004);
+(23001, 23001, 'user:add', '添加用户', null),
+(23002, 23002, 'user:delete', '删除用户', null),
+(23003, 23003, 'user:edit', '修改用户', null),
+(23004, 23004, 'user:query', '查询用户', null);
 
--- 系统初始角色 --
-INSERT IGNORE INTO tb_iam_role(`id`, `identifier`, `name`, `description`, `active`, `sort_id`) VALUE
-(200001, 'super_admin', '超级管理员', null, 1, 200001);
+
+-- ##### Role ##### --
+INSERT IGNORE INTO tb_iam_role(`id`, `sort_id`, `identifier`, `name`, `description`, `active`) VALUE
+(20001, 20001, 'super_admin', '超级管理员', null, 1);
 INSERT IGNORE INTO tb_iam_role_permission(`role_id`, `permission_id`) VALUE
 -- 菜单/路由权限 --
-(200001, 200001),
-(200001, 200002),
-(200001, 200003),
-(200001, 200004),
+(20001, 20001),
+(20001, 20002),
+(20001, 20003),
+(20001, 20004),
 -- 权限 --
-(200001, 201001),
-(200001, 201002),
-(200001, 201003),
-(200001, 201004),
+(20001, 21001),
+(20001, 21002),
+(20001, 21003),
+(20001, 21004),
 -- 角色 --
-(200001, 202001),
-(200001, 202002),
-(200001, 202003),
-(200001, 202004),
+(20001, 22001),
+(20001, 22002),
+(20001, 22003),
+(20001, 22004),
 -- 用户 --
-(200001, 203001),
-(200001, 203002),
-(200001, 204003),
-(200001, 203004);
+(20001, 23001),
+(20001, 23002),
+(20001, 24003),
+(20001, 23004);
 
--- 系统初始用户 --
+
+-- ##### User ##### --
 INSERT IGNORE INTO tb_iam_user(`id`, `username`, `password`, `salt`, `register_time`, `active`) VALUE
-(200001, 'superadmin', '8176126c91f53981449d7575bafa1253', '6386718be1b3', '2025-01-01 00:00:00', 1);   -- superadmin / superadmin
-INSERT IGNORE INTO tb_iam_profile(`user_id`, `avatar`, `nickname`, `photo`, `name`, `gender`, `employee_no`, `phone`, `email`, `main_department_id`, `main_position_id`) VALUE
-(200001, null, '超级管理员', null, '超级管理员', null, null, null, null, null, null);
+-- superadmin / superadmin
+(20001, 'superadmin', '8176126c91f53981449d7575bafa1253', '6386718be1b3', '2025-01-01 00:00:00', 1);
 INSERT IGNORE INTO tb_iam_user_role(`user_id`, `role_id`) VALUE
-(200001, 200001);   -- 超级管理员 <--> 超级管理员
+-- superadmin
+(20001, 20001);
