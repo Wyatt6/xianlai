ec0aa8b6399bce5af1eaa1cab14b7dba3134571b
WyattLau <wyatt6@163.com>
2025-10-23 16:31:08 +0800

    优化

--- xianlai-app-iam/src/main/java/fun/xianlai/app/iam/controller/PermissionController.java
+++ xianlai-app-iam/src/main/java/fun/xianlai/app/iam/controller/PermissionController.java
@@ -73,8 +73,8 @@ public class PermissionController {
      * @return {pageNum 页码, pageSize 页大小, totalPages 页码总数, totalElements 总条数, content 分页数据}
      */
     @ApiLog("条件查询权限分页")
-//    @SaCheckLogin
-//    @SaCheckPermission("permission:query")
+    @SaCheckLogin
+    @SaCheckPermission("permission:query")
     @PostMapping("/getPageConditionally")
     public RetResult getPageConditionally(@RequestParam int pageNum,
                                           @RequestParam int pageSize,
