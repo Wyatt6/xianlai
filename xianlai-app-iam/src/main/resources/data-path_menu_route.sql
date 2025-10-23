91018e0c0a8a302a0d4a23b9ca58e36d1839acf2
WyattLau <wyatt6@163.com>
2025-10-23 09:59:59 +0800

    优化

--- xianlai-app-iam/src/main/resources/data-path_menu_route.sql
+++ xianlai-app-iam/src/main/resources/data-path_menu_route.sql
@@ -1,18 +1,29 @@
-INSERT IGNORE INTO tb_common_sys_path(`name`, `path`, `sort_id`) VALUE
-('IAM', '/iam', 200001),
-('IAM_REDIRECT', '/iam/user-manage', 200002),
-('IAM_USER_MANAGE', '/iam/user-manage', 200003),
-('IAM_ROLE_MANAGE', '/iam/role-manage', 200004),
-('IAM_PERMISSION_MANAGE', '/iam/permission-manage', 200005);
+-- ###### SysPath ##### --
+INSERT IGNORE INTO tb_common_sys_path(`id`, `sort_id`, `name`, `path`) VALUE
+(20001, 20001, 'IAM', '/iam'),
+(20002, 20002, 'IAM_REDIRECT', '/iam/user-manage'),
+(20003, 20003, 'IAM_USER_MANAGE', '/iam/user-manage'),
+(20004, 20004, 'IAM_ROLE_MANAGE', '/iam/role-manage'),
+(20005, 20005, 'IAM_PERMISSION_MANAGE', '/iam/permission-manage');
 
-INSERT IGNORE INTO tb_common_sys_menu(`id`, `sort_id`, `icon`, `title`, `path_name`, `need_permission`, `permission`, `active`, `parent_id`) VALUE
-(200001, 200001, 'ri-group-fill', '身份认证和访问管理', 'IAM', 1, 'menu:iam', 1, 0),
-(200002, 200002, 'ri-user-settings-fill', '用户管理', 'IAM_USER_MANAGE', 1, 'menu:iam_user_manage', 1, 200001),
-(200003, 200003, 'ri-account-box-fill', '角色管理', 'IAM_ROLE_MANAGE', 1, 'menu:iam_role_manage', 1, 200001),
-(200004, 200004, 'ri-shield-keyhole-fill', '权限管理', 'IAM_PERMISSION_MANAGE', 1, 'menu:iam_permission_manage', 1, 200001);
 
-INSERT IGNORE INTO tb_common_sys_route(`id`, `sort_id`, `name`, `path_name`, `redirect_path_name`, `component_path`, `need_login`, `need_permission`, `permission`, `show_tag`, `tag_title`, `parent_id`) VALUE
-(200001, 200001, 'iam', 'IAM', 'IAM_REDIRECT', 'layout/components/Placeholder/index.vue', 1, 1, 'menu:iam', 0, null, 100005),
-(200002, 200002, 'iam_user_manage', 'IAM_USER_MANAGE', null, 'iam/user_manage/index.vue', 1, 1, 'menu:iam_user_manage', 1, '用户管理', 200001),
-(200003, 200003, 'iam_role_manage', 'IAM_ROLE_MANAGE', null, 'iam/role_manage/index.vue', 1, 1, 'menu:iam_role_manage', 1, '角色管理', 200001),
-(200004, 200004, 'iam_permission_manage', 'IAM_PERMISSION_MANAGE', null, 'iam/permission_manage/index.vue', 1, 1, 'menu:iam_permission_manage', 1, '权限管理', 200001);
+-- ###### SysMenu ##### --
+INSERT IGNORE INTO tb_common_sys_menu(
+    `id`, `sort_id`, `parent_id`, `icon`, `title`,
+    `path_name`, `need_permission`, `permission`, `active`
+) VALUE
+(20001, 20001, 0, 'ri-group-fill', '身份认证和访问管理', 'IAM', 1, 'menu:iam', 1);
+(20002, 20002, 20001, 'ri-user-settings-fill', '用户管理', 'IAM_USER_MANAGE', 1, 'menu:iam_user_manage', 1);
+(20003, 20003, 20001, 'ri-account-box-fill', '角色管理', 'IAM_ROLE_MANAGE', 1, 'menu:iam_role_manage', 1);
+(20004, 20004, 20001, 'ri-shield-keyhole-fill', '权限管理', 'IAM_PERMISSION_MANAGE', 1, 'menu:iam_permission_manage', 1);
+
+
+-- ###### SysRoute ##### --
+INSERT IGNORE INTO tb_common_sys_route(
+    `id`, `sort_id`, `parent_id`, `name`, `path_name`, `redirect_path_name`, `component_path`,
+    `need_login`, `need_permission`, `permission`, `show_tag`, `tag_title`
+) VALUE
+(20001, 20001, 0, 'iam', 'IAM', 'IAM_REDIRECT', 'layout/components/Placeholder/index.vue', 1, 1, 'menu:iam', 0, null),
+(20002, 20002, 20001, 'iam_user_manage', 'IAM_USER_MANAGE', null, 'iam/user_manage/index.vue', 1, 1, 'menu:iam_user_manage', 1, '用户管理'),
+(20003, 20003, 20001, 'iam_role_manage', 'IAM_ROLE_MANAGE', null, 'iam/role_manage/index.vue', 1, 1, 'menu:iam_role_manage', 1, '角色管理'),
+(20004, 20004, 20001, 'iam_permission_manage', 'IAM_PERMISSION_MANAGE', null, 'iam/permission_manage/index.vue', 1, 1, 'menu:iam_permission_manage', 1, '权限管理');
