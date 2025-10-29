56734b0e2e91461674e2d1c113c62f874233680d
WyattLau <wyatt6@163.com>
2025-10-30 00:13:56 +0800

    系统设置菜单

--- xianlai-app-common/src/main/resources/data-path_menu_route.sql
+++ xianlai-app-common/src/main/resources/data-path_menu_route.sql
@@ -16,11 +16,12 @@ INSERT IGNORE INTO tb_common_sys_path(`id`, `sort_id`, `name`, `path`) VALUE
 (12004, 12004, 'NOT_CONNECTED', '/500'),
 -- 系统设置
 (13001, 13001, 'SETTING', '/setting'),
-(13002, 13002, 'SETTING_PATH', '/setting/path'),
-(13003, 13003, 'SETTING_MENU', '/setting/menu'),
-(13004, 13004, 'SETTING_ROUTE', '/setting/route'),
-(13005, 13005, 'SETTING_OPTION', '/setting/option'),
-(13006, 13006, 'SETTING_API', '/setting/api'),
+(13002, 13002, 'SETTING_REDIRECT', '/setting/path'),
+(13003, 13003, 'SETTING_PATH', '/setting/path'),
+(13004, 13004, 'SETTING_MENU', '/setting/menu'),
+(13005, 13005, 'SETTING_ROUTE', '/setting/route'),
+(13006, 13006, 'SETTING_OPTION', '/setting/option'),
+(13007, 13007, 'SETTING_API', '/setting/api'),
 -- 最终无匹配 --
 (99999, 99999, 'FINAL_NOT_MATCH', '/:catchAll(.*)');
 
@@ -33,12 +34,12 @@ INSERT IGNORE INTO tb_common_sys_menu(
 -- 首页
 (10001, 10001, 0, 'ri-home-4-fill', '首页', 'HOMEPAGE', 0, null, 1),
 -- 系统设置
-(90001, 90001, 0, 'ri-settings-3-fill', '系统设置', 'SETTING', 0, null, 1),
-(90002, 90002, 90001, 'ri-character-recognition-fill', '路径常量', 'SETTING_PATH', 0, null, 1),
-(90003, 90003, 90001, 'ri-side-bar-fill', '菜单管理', 'SETTING_MENU', 0, null, 1),
-(90004, 90004, 90001, 'ri-navigation-fill', '路由配置', 'SETTING_ROUTE', 0, null, 1),
-(90005, 90005, 90001, 'ri-file-check-fill', '参数选项', 'SETTING_OPTION', 0, null, 1),
-(90006, 90006, 90001, 'ri-puzzle-2-fill', '后端接口', 'SETTING_API', 0, null, 1);
+(90001, 90001, 0, 'ri-settings-3-fill', '系统设置', 'SETTING', 1, "menu:setting", 1),
+(90002, 90002, 90001, 'ri-character-recognition-fill', '路径常量', 'SETTING_PATH', 1, "menu:setting_path", 1),
+(90003, 90003, 90001, 'ri-side-bar-fill', '菜单管理', 'SETTING_MENU', 1, "menu:setting_menu", 1),
+(90004, 90004, 90001, 'ri-navigation-fill', '路由配置', 'SETTING_ROUTE', 1, "menu:setting_route", 1),
+(90005, 90005, 90001, 'ri-file-check-fill', '参数选项', 'SETTING_OPTION', 1, "menu:setting_option", 1),
+(90006, 90006, 90001, 'ri-puzzle-2-fill', '后端接口', 'SETTING_API', 1, "menu:setting_api", 1);
 
 
 -- ###### SysRoute ##### --
@@ -55,6 +56,12 @@ INSERT IGNORE INTO tb_common_sys_route(
 (11001, 11001, 0, 'layout', 'INDEX', 'INDEX_REDIRECT', 'layout/index.vue', 1, 0, null, 0, null),
 (11002, 11002, 11001, 'homepage', 'HOMEPAGE', null, 'homepage/index.vue', 1, 0, null, 0, null),
 -- 系统设置 --
+(90001, 90001, 11001, 'setting', 'SETTING', 'SETTING_REDIRECT', 'layout/components/Placeholder/index.vue', 1, 1, "menu:setting", 0, null),
+(90002, 90002, 90001, 'setting_path', 'SETTING_PATH', null, 'setting/path_manage/index.vue', 1, 1, "menu:setting_path", 1, "路径常量"),
+(90003, 90003, 90001, 'setting_menu', 'SETTING_MENU', null, 'setting/menu_manage/index.vue', 1, 1, "menu:setting_menu", 1, "菜单管理"),
+(90004, 90004, 90001, 'setting_route', 'SETTING_ROUTE', null, 'setting/route_manage/index.vue', 1, 1, "menu:setting_route", 1, "路由配置"),
+(90005, 90005, 90001, 'setting_option', 'SETTING_OPTION', null, 'setting/option_manage/index.vue', 1, 1, "menu:setting_option", 1, "参数选项"),
+(90006, 90006, 90001, 'setting_api', 'SETTING_API', null, 'setting/api_manage/index.vue', 1, 1, "menu:setting_api", 1, "后台接口"),
 -- 错误 --
 (99001, 99001, 11001, 'not_authorized_embedded', 'NOT_AUTHORIZED_EMBEDDED', null, 'errors/403.vue', 1, 0, null, 0, null),
 (99002, 99002, 0, 'not_authorized', 'NOT_AUTHORIZED', null, 'errors/403.vue', 1, 0, null, 0, null),
