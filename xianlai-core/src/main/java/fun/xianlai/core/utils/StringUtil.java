acb255836f8e2851ff2313b946970eb47fb50ec2
WyattLau <wyatt6@163.com>
2025-10-31 22:36:41 +0800

    Create StringUtil.java

---
+++ xianlai-core/src/main/java/fun/xianlai/core/utils/StringUtil.java
@@ -0,0 +1,10 @@
+package fun.xianlai.core.utils;
+
+/**
+ * @author WyattLau
+ */
+public class StringUtil {
+    public static String trim(String str) {
+        return str == null ? null : str.trim();
+    }
+}
