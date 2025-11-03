205c77af249675e17db5c6736a5100514a077493
WyattLau <wyatt6@163.com>
2025-11-03 21:29:40 +0800

    Update data-user.sql

--- xianlai-app-iam/src/main/resources/data-user.sql
+++ xianlai-app-iam/src/main/resources/data-user.sql
@@ -1,5 +1,5 @@
 INSERT IGNORE INTO tb_iam_user(`id`, `username`, `password`, `salt`, `register_time`, `active`, `is_delete`) VALUE
-(1, 'superadmin', '8176126c91f53981449d7575bafa1103', '6386718be1b3', '2010-01-01 00:00:00', 1, 0); -- 账号密码: superadmin / superadmin
+(1, 'superadmin', 'ef2d1f4cc5c516f2c89de844c4861e73', '23fd67871e52', '2010-01-01 00:00:00', 1, 0); -- 账号密码: superadmin / superadmin
 
 INSERT IGNORE INTO tb_iam_user_role(`user_id`, `role_id`) VALUE
 (1, 20001);
