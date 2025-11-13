22fa2113eddd83206143aea16da797a2e75f59c5
WyattLau <wyatt6@163.com>
2025-11-13 10:03:41 +0800

    使用更好的字符串处理工具

---
+++ xianlai-core/src/main/java/fun/xianlai/core/utils/StringUtils.java
@@ -0,0 +1,8 @@
+package fun.xianlai.core.utils;
+
+/**
+ * @author WyattLau
+ */
+public class StringUtils extends org.apache.commons.lang3.StringUtils {
+    // Apache Commons Lang3里的StringUtils包功能很全面，直接使用即可，这里只是占位用
+}
