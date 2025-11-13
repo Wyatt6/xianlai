a97739ed739c1bc4fda181a25fed55abbcc9cf41
WyattLau <wyatt6@163.com>
2025-11-13 22:27:35 +0800

    Update StringUtils.java

--- xianlai-core/src/main/java/fun/xianlai/core/utils/StringUtils.java
+++ xianlai-core/src/main/java/fun/xianlai/core/utils/StringUtils.java
@@ -1,8 +1,9 @@
 package fun.xianlai.core.utils;
 
 /**
+ * 复用org.apache.commons.lang3.StringUtils里的工具
+ *
  * @author WyattLau
  */
 public class StringUtils extends org.apache.commons.lang3.StringUtils {
-    // Apache Commons Lang3里的StringUtils包功能很全面，直接使用即可，这里只是占位用
 }
