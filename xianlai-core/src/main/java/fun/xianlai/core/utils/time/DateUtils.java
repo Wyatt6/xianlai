f1af9abb1b6de0ef616891812246310b565637d4
WyattLau <wyatt6@163.com>
2025-11-13 22:27:15 +0800

    优化

--- xianlai-core/src/main/java/fun/xianlai/core/utils/time/DateUtils.java
+++ xianlai-core/src/main/java/fun/xianlai/core/utils/time/DateUtils.java
@@ -1,31 +1,23 @@
 package fun.xianlai.core.utils.time;
 
-import java.text.SimpleDateFormat;
 import java.util.Date;
 
 /**
  * 复用org.apache.commons.lang3.StringUtils里的工具
+ *
  * @author WyattLau
  */
 public class DateUtils extends org.apache.commons.lang3.time.DateUtils {
-    private static final String COMMON_PATTERN = "yyy-MM-dd HH:mm:ss.SSS";
-
     /**
-     * 常用日期格式：yyy-MM-dd HH:mm:ss.SSS
-     *
-     * @param date 日期
-     * @return 格式化日期字符串
+     * 获取当前时间
      */
-    public static String commonFormat(Date date) {
-        if (date == null) return "null";
-        SimpleDateFormat sdf = new SimpleDateFormat(COMMON_PATTERN);
-        return sdf.format((date));
-    }
-
     public static Date now() {
         return new Date();
     }
 
+    /**
+     * 获取最小时间
+     */
     public static Date zero() {
         return new Date(0);
     }
