9fc57eaec4a583e927e87b7f733c0ab69e5e0bac
WyattLau <wyatt6@163.com>
2025-11-13 22:33:08 +0800

    优化

---
+++ xianlai-core/src/main/java/fun/xianlai/core/utils/file/FileUtils.java
@@ -0,0 +1,9 @@
+package fun.xianlai.core.utils.file;
+
+/**
+ * 复用org.apache.commons.io.FileUtils里的工具
+ *
+ * @author WyattLau
+ */
+public class FileUtils extends org.apache.commons.io.FileUtils {
+}
---
+++ xianlai-core/src/main/java/fun/xianlai/core/utils/file/FilenameUtils.java
@@ -0,0 +1,9 @@
+package fun.xianlai.core.utils.file;
+
+/**
+ * 复用org.apache.commons.io.FilenameUtils里的工具
+ *
+ * @author WyattLau
+ */
+public class FilenameUtils extends org.apache.commons.io.FilenameUtils {
+}
