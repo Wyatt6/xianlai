e3768f5dac6b8d98886e0ab5ed31a49d99f03445
WyattLau <wyatt6@163.com>
2025-11-14 23:08:16 +0800

    Create BeanUtils.java

---
+++ xianlai-core/src/main/java/fun/xianlai/core/utils/BeanUtils.java
@@ -0,0 +1,9 @@
+package fun.xianlai.core.utils;
+
+/**
+ * 复用org.springframework.beans.BeanUtils里的工具
+ *
+ * @author WyattLau
+ */
+public class BeanUtils extends org.springframework.beans.BeanUtils {
+}
