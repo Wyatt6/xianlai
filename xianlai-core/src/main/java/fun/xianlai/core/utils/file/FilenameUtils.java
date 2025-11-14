0d7a653be456feeedfd2a2053f6f27093a23c3e9
WyattLau <wyatt6@163.com>
2025-11-14 22:18:06 +0800

    Update FilenameUtils.java

--- xianlai-core/src/main/java/fun/xianlai/core/utils/file/FilenameUtils.java
+++ xianlai-core/src/main/java/fun/xianlai/core/utils/file/FilenameUtils.java
@@ -1,9 +1,24 @@
 package fun.xianlai.core.utils.file;
 
+import fun.xianlai.core.utils.StringUtils;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.util.Objects;
+
 /**
  * 复用org.apache.commons.io.FilenameUtils里的工具
  *
  * @author WyattLau
  */
 public class FilenameUtils extends org.apache.commons.io.FilenameUtils {
+    /**
+     * 获取文件的拓展名
+     */
+    public static String getExtensionOfFile(MultipartFile file) {
+        String extension = org.apache.commons.io.FilenameUtils.getExtension(file.getOriginalFilename());
+        if (StringUtils.isBlank(extension)) {
+            extension = MimeTypeUtils.getExtension(Objects.requireNonNull(file.getContentType()));
+        }
+        return extension;
+    }
 }
