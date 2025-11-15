b126e010cb587cbc12e06988904410dfaf41eaf0
WyattLau <wyatt6@163.com>
2025-11-15 08:38:13 +0800

    Update EntityUtil.java

--- xianlai-core/src/main/java/fun/xianlai/core/utils/EntityUtil.java
+++ xianlai-core/src/main/java/fun/xianlai/core/utils/EntityUtil.java
@@ -10,32 +10,6 @@ import java.lang.reflect.Method;
  * @author WyattLau
  */
 public class EntityUtil {
-    public static void convert(Object fromObj, Object toObj) {
-        Field[] fromFields = fromObj.getClass().getDeclaredFields();
-        for (Field fromField : fromFields) {
-            try {
-                Field toField = toObj.getClass().getDeclaredField(fromField.getName());
-
-                String fieldName = fromField.getName();
-                String fromGetterName = "get" + fieldName.substring(0, 1).toUpperCase() + fieldName.substring(1);
-                if (boolean.class.getName().equals(fromField.getType().getName())) {
-                    fromGetterName = "is" + fieldName.substring(0, 1).toUpperCase() + fieldName.substring(1);
-                }
-                Method fromGetter = fromObj.getClass().getMethod(fromGetterName);
-
-                String toSetterName = "set" + fieldName.substring(0, 1).toUpperCase() + fieldName.substring(1);
-                Method toSetter = toObj.getClass().getMethod(toSetterName, toField.getType());
-
-                toSetter.invoke(toObj, fromGetter.invoke(fromObj));
-            } catch (NoSuchFieldException ignored) {
-            } catch (NoSuchMethodException e) {
-                throw new SysException("无法找到getter/setter");
-            } catch (InvocationTargetException | IllegalAccessException e) {
-                throw new SysException("调用失败");
-            }
-        }
-    }
-
     public static void convertNotNull(Object fromObj, Object toObj) {
         Field[] fromFields = fromObj.getClass().getDeclaredFields();
         for (Field fromField : fromFields) {
