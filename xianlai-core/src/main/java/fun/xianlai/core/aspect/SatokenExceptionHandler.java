e1d8a1312feefa3c6ad7eebbd36950fdb27c74d1
WyattLau <wyatt6@163.com>
2025-12-02 16:00:12 +0800

    Create SatokenExceptionHandler.java

---
+++ xianlai-core/src/main/java/fun/xianlai/core/aspect/SatokenExceptionHandler.java
@@ -0,0 +1,49 @@
+package fun.xianlai.core.aspect;
+
+import cn.dev33.satoken.exception.NotLoginException;
+import cn.dev33.satoken.exception.NotPermissionException;
+import cn.dev33.satoken.exception.NotRoleException;
+import com.alibaba.fastjson2.JSONObject;
+import fun.xianlai.core.response.RetResult;
+import lombok.extern.slf4j.Slf4j;
+import org.slf4j.MDC;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+/**
+ * @author WyattLau
+ */
+@Slf4j
+@ControllerAdvice
+public class SatokenExceptionHandler {
+    @ExceptionHandler(NotLoginException.class)
+    @ResponseBody
+    public RetResult notLoginExceptionHandler(Exception e) {
+        log.info("Sa-Token验证异常：未登录");
+        RetResult result = new RetResult();
+        result.fail().setFailCode("401").setFailMessage("用户未登录").setTraceId(MDC.get("traceId"));
+        log.info("响应: {}", JSONObject.toJSONString(result));
+        return result;
+    }
+
+    @ExceptionHandler(NotRoleException.class)
+    @ResponseBody
+    public RetResult notRoleExceptionHandler(Exception e) {
+        log.info("Sa-Token验证异常：角色异常");
+        RetResult result = new RetResult();
+        result.fail().setFailCode("403").setFailMessage("用户权限不足").setTraceId(MDC.get("traceId"));
+        log.info("响应: {}", JSONObject.toJSONString(result));
+        return result;
+    }
+
+    @ExceptionHandler(NotPermissionException.class)
+    @ResponseBody
+    public RetResult notPermissionExceptionHandler(Exception e) {
+        log.info("Sa-Token验证异常：权限异常");
+        RetResult result = new RetResult();
+        result.fail().setFailCode("403").setFailMessage("用户权限不足").setTraceId(MDC.get("traceId"));
+        log.info("响应: {}", JSONObject.toJSONString(result));
+        return result;
+    }
+}
