526e65aa83db8e40e576f106e114ed7cba7e47bf
WyattLau <wyatt6@163.com>
2025-12-02 17:28:10 +0800

    Create PathService.java

---
+++ xianlai-app-common/src/main/java/fun/xianlai/app/common/service/PathService.java
@@ -0,0 +1,13 @@
+package fun.xianlai.app.common.service;
+
+import fun.xianlai.app.common.model.entity.SysPath;
+import fun.xianlai.core.response.DataMap;
+import org.springframework.data.domain.Page;
+
+import java.util.List;
+
+/**
+ * @author WyattLau
+ */
+public interface PathService {
+}
