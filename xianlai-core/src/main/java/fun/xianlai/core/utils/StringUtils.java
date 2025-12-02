c4881b86ed6192643dd483f0033374edca0e7d4c
WyattLau <wyatt6@163.com>
2025-12-02 11:11:36 +0800

    代码走查

--- xianlai-core/src/main/java/fun/xianlai/core/utils/StringUtils.java
+++
@@ -1,9 +0,0 @@
-package fun.xianlai.core.utils;
-
-/**
- * 复用org.apache.commons.lang3.StringUtils里的工具
- *
- * @author WyattLau
- */
-public class StringUtils extends org.apache.commons.lang3.StringUtils {
-}
