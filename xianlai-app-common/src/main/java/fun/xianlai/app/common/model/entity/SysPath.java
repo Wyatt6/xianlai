3996db623970a179b27cdfe08d698db4f5897ddc
WyattLau <wyatt6@163.com>
2025-12-02 16:39:25 +0800

    Create SysPath.java

---
+++ xianlai-app-common/src/main/java/fun/xianlai/app/common/model/entity/SysPath.java
@@ -0,0 +1,54 @@
+package fun.xianlai.app.common.model.entity;
+
+import fun.xianlai.core.utils.bean.PrimaryKeyGenerator;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Index;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.DynamicInsert;
+import org.hibernate.annotations.DynamicUpdate;
+import org.hibernate.annotations.GenericGenerator;
+
+/**
+ * 路径
+ * <p>
+ * 用于注册各个页面访问URL的路径，例如：
+ * PORTAL: '/portal',
+ * LOGIN: '/portal/login',
+ * REGISTER: '/portal/register',
+ * RESET_PASSWORD: '/portal/reset-password'
+ *
+ * @author WyattLau
+ */
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@DynamicInsert
+@DynamicUpdate
+@Table(name = "tb_common_sys_path", indexes = {
+        @Index(columnList = "name", unique = true),
+        @Index(columnList = "path", unique = true),
+        @Index(columnList = "sortId, name") // 第1关键字sortId，第2关键字name
+})
+public class SysPath {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY, generator = "pkGen")
+    @GenericGenerator(name = "pkGen", type = PrimaryKeyGenerator.class)
+    private Long id;
+
+    @Column(columnDefinition = "bigint not null default 1")
+    private Long sortId;
+
+    @Column(columnDefinition = "varchar(255) not null")
+    private String name;
+
+    @Column(columnDefinition = "varchar(1000) not null")
+    private String path;
+}
