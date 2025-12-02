f82c93b0d50403912c4b5e1660d8885451e1ea42
WyattLau <wyatt6@163.com>
2025-12-02 16:27:35 +0800

    Create FeignCaptchaService.java

---
+++ xianlai-core/src/main/java/fun/xianlai/core/feign/consumer/FeignCaptchaService.java
@@ -0,0 +1,14 @@
+package fun.xianlai.core.feign.consumer;
+
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+/**
+ * @author WyattLau
+ */
+@FeignClient(contextId = "xianlai-app-common-captcha", name = "xianlai-app-common", path = "/feign/captcha")
+public interface FeignCaptchaService {
+    @GetMapping("/verifyCaptcha")
+    void verifyCaptcha(@RequestParam String captchaKey, @RequestParam String captcha);
+}
