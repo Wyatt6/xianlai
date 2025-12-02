e09df5f8f1970bbba489cb405ac6124c0f5ac335
WyattLau <wyatt6@163.com>
2025-12-02 15:51:16 +0800

    Create FeignConsumerAspect.java

---
+++ xianlai-core/src/main/java/fun/xianlai/core/aspect/FeignConsumerAspect.java
@@ -0,0 +1,39 @@
+package fun.xianlai.core.aspect;
+
+import lombok.extern.slf4j.Slf4j;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Pointcut;
+import org.springframework.stereotype.Component;
+
+/**
+ * @author WyattLau
+ */
+@Slf4j
+@Aspect
+@Component
+public class FeignConsumerAspect {
+    @Pointcut("execution(* fun.xianlai..*.feign.consumer..*(..))")
+    public void pointcut() {
+    }
+
+    @Around("pointcut()")
+    public Object around(ProceedingJoinPoint joinPoint) throws Throwable {
+        long startTimestamp = System.currentTimeMillis();
+        log.info("---->>>> Call Feign Producer [{}] in [{}]", joinPoint.getSignature().getName(), joinPoint.getSignature().getDeclaringTypeName());
+        try {
+            Object result = joinPoint.proceed();
+            log.info("调用结果: {}", result);
+            log.info("调用耗时: {}ms", System.currentTimeMillis() - startTimestamp);
+            log.info("<<<<---- Exit Feign Producer [{}]", joinPoint.getSignature().getName());
+            return result;
+        } catch (Throwable e) {
+            // Feign会把异常封装到DecodeException里，原异常就保存在cause里
+            log.info("出现异常: {} {}", e.getCause().getMessage(), e.getCause().getClass().getName());
+            log.info("调用耗时: {}ms", System.currentTimeMillis() - startTimestamp);
+            log.info("<<<<---- Exit Feign Producer [{}] with Exception", joinPoint.getSignature().getName());
+            throw e.getCause(); // 需要继续向调用该Service的上层Service或Controller抛出异常，不能拦截在这里形成无返回的情况
+        }
+    }
+}
