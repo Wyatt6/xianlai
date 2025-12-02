e53e8616e27d3cdc94686dfe46468a547c61d342
WyattLau <wyatt6@163.com>
2025-12-02 17:22:28 +0800

    Create ApiService.java

---
+++ xianlai-app-common/src/main/java/fun/xianlai/app/common/service/ApiService.java
@@ -0,0 +1,22 @@
+package fun.xianlai.app.common.service;
+
+import fun.xianlai.app.common.model.entity.SysApi;
+import fun.xianlai.core.response.DataMap;
+import org.springframework.data.domain.Page;
+
+import java.util.List;
+
+/**
+ * @author WyattLau
+ */
+public interface ApiService {
+    /**
+     * 缓存接口
+     */
+    void cacheApis();
+
+    /**
+     * 从缓存获取接口
+     */
+    List<SysApi> getApisFromCache();
+}
