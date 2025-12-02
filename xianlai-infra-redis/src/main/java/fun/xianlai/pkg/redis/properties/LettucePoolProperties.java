6444c83d25cf90d218945d91dd694faebfc6d53c
WyattLau <wyatt6@163.com>
2025-12-02 15:23:20 +0800

    Create LettucePoolProperties.java

---
+++ xianlai-infra-redis/src/main/java/fun/xianlai/pkg/redis/properties/LettucePoolProperties.java
@@ -0,0 +1,18 @@
+package fun.xianlai.pkg.redis.properties;
+
+import lombok.Data;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+
+/**
+ * @author WyattLau
+ */
+@Data
+@Component
+@ConfigurationProperties("datasource.redis.lettuce.pool")
+public class LettucePoolProperties {
+    private int minIdle = 1;
+    private int maxIdle = 10;
+    private int maxTotal = 10;
+    private long maxWait = -1L;
+}
