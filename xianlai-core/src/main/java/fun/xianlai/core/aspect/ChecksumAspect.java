95db253726a595c7f4db0e0c559431e0aa9a45c8
WyattLau <wyatt6@163.com>
2025-12-02 16:09:27 +0800

    Create ChecksumAspect.java

---
+++ xianlai-core/src/main/java/fun/xianlai/core/aspect/ChecksumAspect.java
@@ -0,0 +1,56 @@
+package fun.xianlai.core.aspect;
+
+import fun.xianlai.core.response.RetResult;
+import lombok.extern.slf4j.Slf4j;
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.AfterReturning;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Pointcut;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.annotation.Order;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.stereotype.Component;
+
+/**
+ * @author WyattLau
+ */
+@Slf4j
+@Aspect
+@Component
+@Order(2)
+public class ChecksumAspect {
+    @Autowired
+    private RedisTemplate<String, Object> redis;
+
+    @Pointcut("execution(* fun.xianlai..*.controller..*(..))")
+    public void pointcut() {
+    }
+
+    @AfterReturning(pointcut = "pointcut()", returning = "result")
+    public void assembleChecksum(JoinPoint joinPoint, RetResult result) {
+        String optionsChecksum = (String) redis.opsForValue().get("optionsChecksum");
+        if (optionsChecksum != null) {
+            result.addChecksum("optionsChecksum", optionsChecksum);
+        }
+
+        String pathsChecksum = (String) redis.opsForValue().get("pathsChecksum");
+        if (pathsChecksum != null) {
+            result.addChecksum("pathsChecksum", pathsChecksum);
+        }
+
+        String menusChecksum = (String) redis.opsForValue().get("menusChecksum");
+        if (menusChecksum != null) {
+            result.addChecksum("menusChecksum", menusChecksum);
+        }
+
+        String apisChecksum = (String) redis.opsForValue().get("apisChecksum");
+        if (apisChecksum != null) {
+            result.addChecksum("apisChecksum", apisChecksum);
+        }
+
+        String routesChecksum = (String) redis.opsForValue().get("routesChecksum");
+        if (routesChecksum != null) {
+            result.addChecksum("routesChecksum", routesChecksum);
+        }
+    }
+}
