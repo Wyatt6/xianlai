9204eb13d312c2441414997080eb5848f5e170d9
WyattLau <wyatt6@163.com>
2025-12-03 23:56:53 +0800

    Create UserInfo.java

---
+++ xianlai-app-iam/src/main/java/fun/xianlai/app/iam/model/entity/other/UserInfo.java
@@ -0,0 +1,61 @@
+package fun.xianlai.app.iam.model.entity.other;
+
+import fun.xianlai.app.iam.model.entity.rbac.User;
+import fun.xianlai.core.exception.SysException;
+import fun.xianlai.core.utils.bean.BeanUtils;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.Date;
+
+/**
+ * @author WyattLau
+ */
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class UserInfo {
+    // User
+    private Long id;
+    private String username;
+    private Date registerTime;
+    private Boolean active;
+    private Boolean isDelete;
+    // Profile
+    private String avatar;
+    private String nickname;
+    private String gender;
+    private String phone;
+    private String email;
+
+    public User exportUser() {
+        User user = new User();
+        BeanUtils.copyProperties(this, user);
+        return user;
+    }
+
+    public void importUser(User user) {
+        if (this.id != null && !this.id.equals(user.getId())) {
+            throw new SysException("用户信息错误");
+        } else {
+            BeanUtils.copyProperties(user, this);
+        }
+    }
+
+    public Profile exportProfile() {
+        Profile profile = new Profile();
+        BeanUtils.copyProperties(this, profile);
+        profile.setUserId(this.id);
+        return profile;
+    }
+
+    public void importProfile(Profile profile) {
+        if (this.id != null && !this.id.equals(profile.getUserId())) {
+            throw new SysException("用户信息错误");
+        } else {
+            BeanUtils.copyProperties(profile, this);
+            this.id = profile.getUserId();
+        }
+    }
+}
