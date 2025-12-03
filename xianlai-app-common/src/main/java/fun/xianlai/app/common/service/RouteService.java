fb757c98663bd5b9665cb30d77f443ffa8e8b781
WyattLau <wyatt6@163.com>
2025-12-03 09:38:07 +0800

    Create RouteService.java

---
+++ xianlai-app-common/src/main/java/fun/xianlai/app/common/service/RouteService.java
@@ -0,0 +1,42 @@
+package fun.xianlai.app.common.service;
+
+import fun.xianlai.app.common.model.entity.SysRoute;
+import fun.xianlai.core.response.DataMap;
+
+import java.util.List;
+import java.util.Map;
+
+/**
+ * @author WyattLau
+ */
+public interface RouteService {
+    /**
+     * 缓存路由
+     */
+    void cacheRoutes();
+
+    /**
+     * 从缓存获取路由
+     */
+    List<Map<String, Object>> getRoutesFromCache();
+
+    /**
+     * 新增路由
+     */
+    DataMap add(SysRoute route);
+
+    /**
+     * 删除路由
+     */
+    void delete(Long routeId);
+
+    /**
+     * 修改路由
+     */
+    DataMap edit(SysRoute route);
+
+    /**
+     * 获取路由森林
+     */
+    List<SysRoute> getRouteForest();
+}
