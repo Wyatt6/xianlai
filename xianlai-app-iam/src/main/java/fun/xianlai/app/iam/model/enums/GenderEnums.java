920bc6cca354f09f169a16a84c7de80c0d9b5a59
WyattLau <wyatt6@163.com>
2025-12-04 00:05:26 +0800

    Create GenderEnums.java

---
+++ xianlai-app-iam/src/main/java/fun/xianlai/app/iam/model/enums/GenderEnums.java
@@ -0,0 +1,10 @@
+package fun.xianlai.app.iam.model.enums;
+
+/**
+ * @author WyattLau
+ */
+public class GenderEnums {
+    public static final String UNKNOWN = "unknown";
+    public static final String MALE = "male";
+    public static final String FEMALE = "female";
+}
