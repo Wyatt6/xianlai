4ba109a1003fdef306821c6fb00e7ccc452a7fca
WyattLau <wyatt6@163.com>
2025-12-03 09:23:00 +0800

    Update MenuController.java

--- xianlai-app-common/src/main/java/fun/xianlai/app/common/controller/MenuController.java
+++ xianlai-app-common/src/main/java/fun/xianlai/app/common/controller/MenuController.java
@@ -35,4 +35,41 @@ public class MenuController {
         BeanUtils.trimString(form);
         return new RetResult().success().setData(menuService.add(form));
     }
+
+    @ApiLog("删除菜单")
+    @SaCheckLogin
+    @SaCheckPermission("menu:delete")
+    @GetMapping("/delete")
+    public RetResult delete(@RequestParam Long menuId) {
+        log.info("请求参数: menuId=[{}]", menuId);
+        menuService.delete(menuId);
+        return new RetResult().success();
+    }
+
+    @ApiLog("修改菜单")
+    @SaCheckLogin
+    @SaCheckPermission("menu:edit")
+    @PostMapping("/edit")
+    public RetResult edit(@RequestBody SysMenu form) {
+        log.info("请求参数: {}", form);
+        BeanUtils.trimString(form);
+        return new RetResult().success().setData(menuService.edit(form));
+    }
+
+    @ApiLog("重载菜单缓存")
+    @SaCheckLogin
+    @SaCheckPermission("menu:edit")
+    @GetMapping("/reloadCache")
+    public RetResult reloadCache() {
+        menuService.cacheActiveMenus();
+        return new RetResult().success();
+    }
+
+    @ApiLog("查询菜单森林")
+    @SaCheckLogin
+    @SaCheckPermission("menu:query")
+    @GetMapping("/getForest")
+    public RetResult getForest() {
+        return new RetResult().success().addData("menus", menuService.getMenuForest());
+    }
 }
