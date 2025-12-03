604bb9ef9606ce837327b0dfb7ea7dc4472c2f49
WyattLau <wyatt6@163.com>
2025-12-03 23:38:27 +0800

    Create Permission.java

---
+++ xianlai-app-iam/src/main/java/fun/xianlai/app/iam/model/entity/rbac/Permission.java
@@ -0,0 +1,51 @@
+package fun.xianlai.app.iam.model.entity.rbac;
+
+import fun.xianlai.core.utils.bean.PrimaryKeyGenerator;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Index;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.DynamicInsert;
+import org.hibernate.annotations.DynamicUpdate;
+import org.hibernate.annotations.GenericGenerator;
+
+/**
+ * RBAC-权限
+ * 由程序运行时自动生成，不允许修改
+ *
+ * @author WyattLau
+ */
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@DynamicInsert
+@DynamicUpdate
+@Table(name = "tb_iam_permission", indexes = {
+        @Index(columnList = "identifier", unique = true),
+        @Index(columnList = "sortId, identifier")   // sortId asc, identifier asc
+})
+public class Permission {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY, generator = "pkGen")
+    @GenericGenerator(name = "pkGen", type = PrimaryKeyGenerator.class)
+    private Long id;
+
+    @Column(columnDefinition = "bigint not null default 0")
+    private Long sortId;
+
+    @Column(columnDefinition = "varchar(255) not null")
+    private String identifier;
+
+    @Column
+    private String name;
+
+    @Column(length = 1000)
+    private String description;
+}
