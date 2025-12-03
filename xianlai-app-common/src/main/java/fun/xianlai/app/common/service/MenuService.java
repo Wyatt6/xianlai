6c4bf52e57e626cd8701ed2d0878a7cf9ed1d7e7
WyattLau <wyatt6@163.com>
2025-12-03 09:23:22 +0800

    Update MenuService.java

--- xianlai-app-common/src/main/java/fun/xianlai/app/common/service/MenuService.java
+++ xianlai-app-common/src/main/java/fun/xianlai/app/common/service/MenuService.java
@@ -11,7 +11,37 @@ import java.util.Map;
  */
 public interface MenuService {
     /**
+     * 缓存生效的菜单
+     */
+    void cacheActiveMenus();
+
+    /**
+     * 从缓存获取生效的菜单
+     */
+    List<Map<String, Object>> getActiveMenusFromCache();
+
+    /**
      * 新增菜单
      */
     DataMap add(SysMenu menu);
+
+    /**
+     * 删除菜单
+     */
+    void delete(Long menuId);
+
+    /**
+     * 修改菜单
+     */
+    DataMap edit(SysMenu menu);
+
+    /**
+     * 获取菜单森林
+     */
+    List<SysMenu> getMenuForest();
+
+    /**
+     * 获取生效中的菜单森林
+     */
+    List<SysMenu> getActiveMenuForest();
 }
