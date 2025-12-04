bfa0c3aae3c368b163e2a70c3400935a97711e76
WyattLau <wyatt6@163.com>
2025-12-04 15:36:30 +0800

    Update UserService.java

--- xianlai-app-iam/src/main/java/fun/xianlai/app/iam/service/UserService.java
+++ xianlai-app-iam/src/main/java/fun/xianlai/app/iam/service/UserService.java
@@ -41,4 +41,30 @@ public interface UserService {
      * @return 用户对象
      */
     User authentication(Long userId, String password);
+
+    /**
+     * 从缓存或数据库获取用户角色
+     *
+     * @param userId 用户ID
+     * @return 生效中的角色标识符列表
+     */
+    List<String> getRoleList(Long userId);
+
+    /**
+     * 从缓存或数据库获取用户权限标
+     *
+     * @param userId 用户ID
+     * @return 生效中的权限标识符列表
+     */
+    List<String> getPermissionList(Long userId);
+
+    /**
+     * 设置roleListCacheTime时间戳
+     */
+    void setRoleListCacheTime(Long userId, Date timestamp);
+
+    /**
+     * 设置permissionListCacheTime时间戳
+     */
+    void setPermissionListCacheTime(Long userId, Date timestamp);
 }
