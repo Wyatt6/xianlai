bad3cd785ffd6c4cec3a219a99d43427cd614e59
WyattLau <wyatt6@163.com>
2025-12-04 15:01:58 +0800

    Create UserService.java

---
+++ xianlai-app-iam/src/main/java/fun/xianlai/app/iam/service/UserService.java
@@ -0,0 +1,25 @@
+package fun.xianlai.app.iam.service;
+
+import fun.xianlai.app.iam.model.entity.other.Profile;
+import fun.xianlai.app.iam.model.entity.rbac.User;
+import fun.xianlai.app.iam.model.form.UserCondition;
+import fun.xianlai.app.iam.model.entity.other.UserInfo;
+import fun.xianlai.core.response.DataMap;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.data.domain.Page;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.util.Date;
+import java.util.List;
+import java.util.Optional;
+
+/**
+ * @author WyattLau
+ */
+public interface UserService {
+
+    boolean matchUsernameFormat(String username);
+
+    boolean matchPasswordFormat(String password);
+
+}
