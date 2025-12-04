5bcdb2c320cb13f3ca2394ec6b486e60021e1274
WyattLau <wyatt6@163.com>
2025-12-04 14:46:26 +0800

    Create RoleServiceImpl.java

---
+++ xianlai-app-iam/src/main/java/fun/xianlai/app/iam/service/impl/RoleServiceImpl.java
@@ -0,0 +1,38 @@
+package fun.xianlai.app.iam.service.impl;
+
+import fun.xianlai.app.iam.model.entity.rbac.Role;
+import fun.xianlai.app.iam.model.entity.rbac.RolePermission;
+import fun.xianlai.app.iam.repository.RolePermissionRepository;
+import fun.xianlai.app.iam.repository.RoleRepository;
+import fun.xianlai.app.iam.repository.UserRoleRepository;
+import fun.xianlai.app.iam.service.PermissionService;
+import fun.xianlai.app.iam.service.RoleService;
+import fun.xianlai.core.annotation.ServiceLog;
+import fun.xianlai.core.annotation.SimpleServiceLog;
+import fun.xianlai.core.exception.SysException;
+import fun.xianlai.core.response.DataMap;
+import fun.xianlai.core.utils.bean.BeanUtils;
+import fun.xianlai.core.utils.time.DateUtils;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+import java.util.Optional;
+
+/**
+ * @author WyattLau
+ */
+@Slf4j
+@Service
+public class RoleServiceImpl implements RoleService {
+}
