664b1bef5c3afdeb90a52a325ed5db0575063495
WyattLau <wyatt6@163.com>
2025-12-04 15:45:00 +0800

    Create CacheInitializer.java

---
+++ xianlai-app-iam/src/main/java/fun/xianlai/app/iam/initializer/CacheInitializer.java
@@ -0,0 +1,29 @@
+package fun.xianlai.app.iam.initializer;
+
+import fun.xianlai.app.iam.service.PermissionService;
+import fun.xianlai.app.iam.service.RoleService;
+import fun.xianlai.core.utils.time.DateUtils;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+/**
+ * @author WyattLau
+ */
+@Slf4j
+@Component
+public class CacheInitializer implements CommandLineRunner {
+    @Autowired
+    private PermissionService permissionService;
+    @Autowired
+    private RoleService roleService;
+
+    @Override
+    public void run(String... args) throws Exception {
+        log.info("开始初始化IAM模块缓存");
+        permissionService.setPermissionDbRefreshTime(DateUtils.now());
+        roleService.setRoleDbRefreshTime(DateUtils.now());
+        log.info("已完成IAM模块缓存初始化");
+    }
+}
