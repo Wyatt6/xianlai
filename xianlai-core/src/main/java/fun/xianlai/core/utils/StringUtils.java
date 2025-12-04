2e4dbbd692d5b43d59967e429da1bf6f9288f80d
WyattLau <wyatt6@163.com>
2025-12-04 16:40:23 +0800

    Create StringUtils.java

---
+++ xianlai-core/src/main/java/fun/xianlai/core/utils/StringUtils.java
@@ -0,0 +1,9 @@
+package fun.xianlai.core.utils;
+
+/**
+ * 复用org.apache.commons.lang3.StringUtils里的工具
+ *
+ * @author WyattLau
+ */
+public class StringUtils extends org.apache.commons.lang3.StringUtils {
+}
