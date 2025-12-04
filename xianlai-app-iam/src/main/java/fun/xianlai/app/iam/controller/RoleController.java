8d9cabb494ab5bd5e7296b4e5383be3a3c21f5f4
WyattLau <wyatt6@163.com>
2025-12-04 09:03:53 +0800

    Update RoleController.java

--- xianlai-app-iam/src/main/java/fun/xianlai/app/iam/controller/RoleController.java
+++ xianlai-app-iam/src/main/java/fun/xianlai/app/iam/controller/RoleController.java
@@ -27,4 +27,72 @@ import java.util.List;
 @RestController
 @RequestMapping("/role")
 public class RoleController {
+    @Autowired
+    private RoleService roleService;
+
+    @ApiLog("新增角色")
+    @SaCheckLogin
+    @SaCheckPermission("role:add")
+    @PostMapping("/add")
+    public RetResult add(@RequestBody Role form) {
+        log.info("请求参数: {}", form);
+        BeanUtils.trimString(form);
+        return new RetResult().success().setData(roleService.add(form));
+    }
+
+    @ApiLog("删除角色")
+    @SaCheckLogin
+    @SaCheckPermission("role:delete")
+    @GetMapping("/delete")
+    public RetResult delete(@RequestParam Long roleId) {
+        log.info("请求参数: roleId=[{}]", roleId);
+        roleService.deleteRole(roleId);
+        return new RetResult().success();
+    }
+
+    @ApiLog("修改角色")
+    @SaCheckLogin
+    @SaCheckPermission("role:edit")
+    @PostMapping("/edit")
+    public RetResult edit(@RequestBody Role form) {
+        log.info("请求参数: {}", form);
+        BeanUtils.trimString(form);
+        return new RetResult().success().setData(roleService.edit(form));
+    }
+
+    /**
+     * 查询条件为空时查询全量数据
+     * 页码<0或页大小<=0时不分页
+     *
+     * @param pageNum   页码
+     * @param pageSize  页大小
+     * @param condition 查询条件
+     * @return {pageNum 页码, pageSize 页大小, totalPages 页码总数, totalElements 总条数, content 分页数据}
+     */
+    @ApiLog("条件查询角色分页")
+    @SaCheckLogin
+    @SaCheckPermission("role:query")
+    @PostMapping("/getPageConditionally")
+    public RetResult getPageConditionally(@RequestParam int pageNum,
+                                          @RequestParam int pageSize,
+                                          @RequestBody(required = false) Role condition) {
+        log.info("请求参数：pageNum=[{}], pageSize=[{}], condition=[{}]", pageNum, pageSize, condition);
+        Page<Role> roles = roleService.getRolesByPageConditionally(pageNum, pageSize, condition);
+        return new RetResult().success()
+                .addData("pageNum", pageNum)
+                .addData("pageSize", pageSize)
+                .addData("totalPages", roles.getTotalPages())
+                .addData("totalElements", roles.getTotalElements())
+                .addData("content", roles.getContent());
+    }
+
+    @ApiLog("查询某用户所具有的角色ID列表")
+    @SaCheckLogin
+    @SaCheckPermission("role:query")
+    @GetMapping("/getRoleIdsOfUser")
+    public RetResult getRoleIdsOfUser(@RequestParam Long userId) {
+        log.info("请求参数: userId=[{}]", userId);
+        return new RetResult().success().addData("roleIds", roleService.getRoleIdsOfUser(userId));
+    }
+
 }
