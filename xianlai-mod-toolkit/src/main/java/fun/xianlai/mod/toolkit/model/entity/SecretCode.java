9a2031c61b410de5d12b0dabe60bd88622e0adec
WyattLau <wyatt6@163.com>
2025-12-18 16:53:29 +0800

    创建密码本实体对象

---
+++ xianlai-mod-toolkit/src/main/java/fun/xianlai/mod/toolkit/model/entity/SecretCode.java
@@ -0,0 +1,74 @@
+package fun.xianlai.mod.toolkit.model.entity;
+
+import fun.xianlai.core.utils.bean.PrimaryKeyGenerator;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Index;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.DynamicInsert;
+import org.hibernate.annotations.DynamicUpdate;
+import org.hibernate.annotations.GenericGenerator;
+
+/**
+ * @author WyattLau
+ */
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@DynamicInsert
+@DynamicUpdate
+@Table(name = "tb_toolkit_secret_code", indexes = {
+        @Index(columnList = "sortId"),
+})
+public class SecretCode {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY, generator = "PK_generator")
+    @GenericGenerator(name = "PK_generator", type = PrimaryKeyGenerator.class)
+    private Long id;
+
+    @Column(columnDefinition = "bigint not null default 0")
+    private Long sortId;
+
+    @Column
+    private String category;
+
+    @Column
+    private String title;
+
+    @Column
+    private String username;
+
+    @Column
+    private String code;
+
+    @Column
+    private String tips;
+
+    @Column(columnDefinition = "bit not null default 0")
+    private Boolean twoFAS;
+
+    @Column(columnDefinition = "bit not null default 0")
+    private Boolean appleId;
+
+    @Column(columnDefinition = "bit not null default 0")
+    private Boolean wechat;
+
+    @Column(columnDefinition = "bit not null default 0")
+    private Boolean alipay;
+
+    @Column
+    private String phone;
+
+    @Column
+    private String email;
+
+    @Column(length = 1000)
+    private String remark;
+}
