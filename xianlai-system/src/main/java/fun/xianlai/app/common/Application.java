397b7a856a41f6266976fa83b65fe707413c8ce5
WyattLau <wyatt6@163.com>
2026-03-03 16:17:47 +0800

    xianlai-app-common改名为xianlai-system

--- xianlai-system/src/main/java/fun/xianlai/app/common/Application.java
+++ xianlai-system/src/main/java/fun/xianlai/app/common/Application.java
@@ -16,6 +16,6 @@ import org.springframework.cloud.openfeign.EnableFeignClients;
 public class Application {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
-        log.info("--------------- 微服务 xianlai-app-common 已启动 ---------------");
+        log.info("--------------- 微服务 xianlai-system 已启动 ---------------");
     }
 }
