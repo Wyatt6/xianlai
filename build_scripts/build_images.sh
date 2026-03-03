a59e47bedf53e023ceb45b9bf2e2f33649bece10
WyattLau <wyatt6@163.com>
2026-03-03 16:29:31 +0800

    xianlai-app-gateway改名为xianlai-gateway

--- build_scripts/build_images.sh
+++ build_scripts/build_images.sh
@@ -1,5 +1,5 @@
 # TODO CBR: 每次发版前检查模块版本号
-xianlai_app_gateway_ver=1.0.0
+xianlai_gateway_ver=1.0.0
 xianlai_system_ver=1.1.0
 xianlai_app_iam_ver=1.1.0
 xianlai_mod_toolkit_ver=1.0.1
@@ -7,8 +7,8 @@ xianlai_mod_toolkit_ver=1.0.1
 # 分布式镜像
 docker build \
   --platform linux/amd64 \
-  --build-arg VERSION=${xianlai_app_gateway_ver} \
-  -t wyatt6/xianlai-app-gateway:${xianlai_app_gateway_ver} \
+  --build-arg VERSION=${xianlai_gateway_ver} \
+  -t wyatt6/xianlai-app-gateway:${xianlai_gateway_ver} \
   ../xianlai-app-gateway/
 
 docker build \
