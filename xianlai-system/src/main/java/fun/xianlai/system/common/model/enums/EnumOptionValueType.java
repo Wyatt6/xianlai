de2fe805a7967c1a8ef2b76e716a57a082de0fcb
WyattLau <wyatt6@163.com>
2026-03-25 11:14:34 +0800

    修复bug

--- xianlai-system/src/main/java/fun/xianlai/system/common/model/enums/EnumOptionValueType.java
+++ xianlai-system/src/main/java/fun/xianlai/system/common/model/enums/EnumOptionValueType.java
@@ -5,7 +5,7 @@ package fun.xianlai.system.common.model.enums;
  */
 public class EnumOptionValueType {
     public static final String STRING = "STRING";
-    public static final String BOOLEAN = "STRING";
+    public static final String BOOLEAN = "BOOLEAN";
     public static final String INTEGER = "INTEGER";
     public static final String LONG = "LONG";
     public static final String JSON = "JSON";       // Array / Object / 复杂结构
