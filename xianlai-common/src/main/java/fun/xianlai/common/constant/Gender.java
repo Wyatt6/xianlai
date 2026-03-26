1406e089bd207b46f9272ceda27bec15b047775d
WyattLau <wyatt6@163.com>
2026-03-26 08:53:31 +0800

    新增性别常量

---
+++ xianlai-common/src/main/java/fun/xianlai/common/constant/Gender.java
@@ -0,0 +1,10 @@
+package fun.xianlai.common.constant;
+
+/**
+ * @author WyattLau
+ */
+public class Gender {
+    public static final String UNKNOWN = "UNKNOWN";
+    public static final String MALE = "MALE";
+    public static final String FEMALE = "FEMALE";
+}
