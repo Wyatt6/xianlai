14a5830a5a32fcf3a32af5c0734b693869cd07aa
WyattLau <wyatt6@163.com>
2026-03-26 08:54:12 +0800

    默认租户数据初始化

--- xianlai-system/src/main/resources/data-tenant.sql
+++ xianlai-system/src/main/resources/data-tenant.sql
@@ -1,2 +1,5 @@
-INSERT IGNORE INTO tb_iam_tenant(`id`, `code`, `name`, `active`) VALUES
-(1, 'default', '默认租户', 1);
+INSERT IGNORE INTO tb_sys_core_tenant(
+       `id`, `code`, `domain`, `status`, `expire_time`, `logo`, `display_name`,
+       `contact_name`, `contact_gender`, `contact_phone`, `contact_email`
+) VALUES
+(1, 'default', null, 'NORMAL', null, null, '默认租户', 'UNKNOWN', null, null, null);
