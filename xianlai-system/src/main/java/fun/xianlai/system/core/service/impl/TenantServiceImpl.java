e1088ba49227f024a4b32c30653ec9817e5979db
WyattLau <wyatt6@163.com>
2026-03-26 17:59:41 +0800

    优化日志打印

--- xianlai-system/src/main/java/fun/xianlai/system/core/service/impl/TenantServiceImpl.java
+++ xianlai-system/src/main/java/fun/xianlai/system/core/service/impl/TenantServiceImpl.java
@@ -1,10 +1,11 @@
 package fun.xianlai.system.core.service.impl;
 
-import fun.xianlai.common.annotation.SimpleServiceLog;
+import fun.xianlai.common.annotation.ServiceLog;
 import fun.xianlai.common.exception.SysException;
 import fun.xianlai.system.core.model.entity.XLTenant;
 import fun.xianlai.system.core.repository.XLTenantRepository;
 import fun.xianlai.system.core.service.TenantService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Service;
@@ -15,6 +16,7 @@ import java.util.Optional;
 /**
  * @author WyattLau
  */
+@Slf4j
 @Service
 public class TenantServiceImpl implements TenantService {
     private static final String TENANT_DOMAIN_KEY = "tenant:domain:";
@@ -26,22 +28,25 @@ public class TenantServiceImpl implements TenantService {
     private XLTenantRepository tenantRepository;
 
     @Override
-    @SimpleServiceLog("根据域名获取租户")
+    @ServiceLog("根据域名获取租户")
     public XLTenant getTenantByDomain(String domain) {
         String key = TENANT_DOMAIN_KEY + domain;
-
+        log.info("根据域名查询缓存");
         XLTenant cachedTenant = (XLTenant) redis.opsForValue().get(key);
         if (cachedTenant != null) {
             redis.expire(key, Duration.ofHours(TENANT_DOMAIN_CACHE_HOURS));
             return cachedTenant;
         }
 
+        log.info("缓存查不到租户数据，再根据域名查询数据库");
         Optional<XLTenant> tenant = tenantRepository.findByDomain(domain);
         if (tenant.isEmpty()) {
+            log.info("域名查不到租户数据，再根据最小子域名查询数据库");
             String subDomain = getFirstSubDomain(domain);
             tenant = tenantRepository.findByCode(subDomain);
         }
         if (tenant.isPresent()) {
+            log.info("成功从数据库查到租户数据，更新缓存");
             redis.opsForValue().set(key, tenant.get(), Duration.ofHours(TENANT_DOMAIN_CACHE_HOURS));
             return tenant.get();
         } else {
