489bb42223361b70d8add7dc8d9804d068c99d72
WyattLau <wyatt6@163.com>
2026-03-27 15:11:02 +0800

    新增：租户上下文

---
+++ xianlai-common/src/main/java/fun/xianlai/common/context/TenantContext.java
@@ -0,0 +1,20 @@
+package fun.xianlai.common.context;
+
+/**
+ * @author WyattLau
+ */
+public class TenantContext {
+    private static final ThreadLocal<Long> TENANT_ID = new ThreadLocal<>();
+
+    public static void setTenantId(Long tenantId) {
+        TENANT_ID.set(tenantId);
+    }
+
+    public static Long getTenantId() {
+        return TENANT_ID.get();
+    }
+
+    public static void clear() {
+        TENANT_ID.remove();
+    }
+}
