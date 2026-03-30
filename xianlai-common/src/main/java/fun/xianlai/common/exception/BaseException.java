7863b733bcb8daa1908ac6fb4d8ffd188a016361
WyattLau <wyatt6@163.com>
2026-03-30 11:02:42 +0800

    新增：异常基类

---
+++ xianlai-common/src/main/java/fun/xianlai/common/exception/BaseException.java
@@ -0,0 +1,16 @@
+package fun.xianlai.common.exception;
+
+import lombok.Getter;
+
+/**
+ * @author WyattLau
+ */
+@Getter
+public class BaseException extends RuntimeException{
+    private final String code;
+
+    public BaseException(String code, String message) {
+        super(message);
+        this.code = code;
+    }
+}
