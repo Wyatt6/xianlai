399eb0cb5477b2310ca84ccd91ce4afb6b8a8e4a
WyattLau <wyatt6@163.com>
2026-03-30 11:14:21 +0800

    优化：规范化系统异常类

--- xianlai-common/src/main/java/fun/xianlai/common/exception/SysException.java
+++ xianlai-common/src/main/java/fun/xianlai/common/exception/SysException.java
@@ -1,18 +1,48 @@
 package fun.xianlai.common.exception;
 
+import fun.xianlai.common.response.RetCode;
+
 /**
  * @author WyattLau
  */
-public class SysException extends RuntimeException {
-    public SysException() {
-        super("系统错误");
+public class SysException extends BaseException {
+    public SysException(String code, String message) {
+        super(code, message);
+    }
+
+    public SysException(String code, String message, Throwable cause) {
+        super(code, message, cause);
     }
 
     public SysException(String message) {
-        super(message);
+        super(RetCode.SYS_ERROR, message);
     }
 
     public SysException(String message, Throwable cause) {
-        super(message, cause);
+        super(RetCode.SYS_ERROR, message, cause);
+    }
+
+    public static SysException dbError(String message) {
+        return new SysException(RetCode.DB_ERROR, message);
+    }
+
+    public static SysException dbError(String message, Throwable cause) {
+        return new SysException(RetCode.DB_ERROR, message, cause);
+    }
+
+    public static SysException cacheError(String message) {
+        return new SysException(RetCode.CACHE_ERROR, message);
+    }
+
+    public static SysException cacheError(String message, Throwable cause) {
+        return new SysException(RetCode.CACHE_ERROR, message, cause);
+    }
+
+    public static SysException thirdPartyError(String message) {
+        return new SysException(RetCode.THIRD_SERVICE_ERROR, message);
+    }
+
+    public static SysException thirdPartyError(String message, Throwable cause) {
+        return new SysException(RetCode.THIRD_SERVICE_ERROR, message, cause);
     }
 }
