a0eb4f08718a16916aed7b9e2b6547f19c664d03
WyattLau <wyatt6@163.com>
2026-03-30 10:17:43 +0800

    新增：性别常量增加OTHER

--- xianlai-common/src/main/java/fun/xianlai/common/constant/Gender.java
+++ xianlai-common/src/main/java/fun/xianlai/common/constant/Gender.java
@@ -7,4 +7,5 @@ public class Gender {
     public static final String UNKNOWN = "UNKNOWN";
     public static final String MALE = "MALE";
     public static final String FEMALE = "FEMALE";
+    public static final String OTHER = "OTHER";
 }
