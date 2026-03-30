023662edc3c0caa9da37fbe3b5e41ee100926241
WyattLau <wyatt6@163.com>
2026-03-30 09:13:37 +0800

    优化：转发报文头增加stTimestamp

--- xianlai-gateway/src/main/java/fun/xianlai/gateway/filter/LogAndTenantFilter.java
+++ xianlai-gateway/src/main/java/fun/xianlai/gateway/filter/LogAndTenantFilter.java
@@ -71,7 +71,7 @@ public class LogAndTenantFilter implements GlobalFilter, Ordered {
             if (tenantId != null) {
                 builder.header("tenantId", tenantId);
             }
-            
+
             // 4. 转发处理和后处理
             ServerHttpRequest redirectRequest = builder.build();
             return chain.filter(exchange.mutate().request(redirectRequest).build()).then(Mono.fromRunnable(() -> {
