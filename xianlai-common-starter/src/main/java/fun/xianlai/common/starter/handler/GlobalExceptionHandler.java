9d777011287e0c31d05364cc2cbc4caedef71dd9
WyattLau <wyatt6@163.com>
2026-03-30 11:55:29 +0800

    新增：全局异常处理器

--- xianlai-common-starter/src/main/java/fun/xianlai/common/starter/handler/GlobalExceptionHandler.java
+++ xianlai-common-starter/src/main/java/fun/xianlai/common/starter/handler/GlobalExceptionHandler.java
@@ -2,39 +2,36 @@ package fun.xianlai.common.starter.handler;
 
 import fun.xianlai.common.exception.BizException;
 import fun.xianlai.common.exception.SysException;
+import fun.xianlai.common.response.RetCode;
+import fun.xianlai.common.response.RetResult;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 /**
  * @author WyattLau
  */
+@Slf4j
 @RestControllerAdvice
 public class GlobalExceptionHandler {
+    /**
+     * 最终兜底的异常处理
+     */
+    @ExceptionHandler(Exception.class)
+    public RetResult<?> handleException(Exception e) {
+        log.error("未知错误：", e);
+        return RetResult.fail(RetCode.SYS_ERROR, "服务器繁忙，请稍后再试");
+    }
+
     @ExceptionHandler(BizException.class)
     public RetResult<?> handleBizException(BizException e) {
-        log.error("业务异常：{}", e.getMessage());
-        return RetResult.fail(e.getCode(), e.getMessage())
-                .setTraceId(getTraceId());
+        log.error("业务错误：{}", e.getMessage());
+        return RetResult.fail(e.getCode(), e.getMessage());
     }
 
     @ExceptionHandler(SysException.class)
     public RetResult<?> handleSysException(SysException e) {
-        log.error("系统异常：", e);
-        return RetResult.fail(e.getCode(), e.getMessage())
-                .setTraceId(getTraceId());
-    }
-
-    @ExceptionHandler(Exception.class)
-    public RetResult<?> handleException(Exception e) {
-        log.error("服务器异常：", e);
-        return RetResult.fail("500", "服务器繁忙，请稍后再试")
-                .setTraceId(getTraceId());
-    }
-
-    private String getTraceId() {
-        try {
-            RequestAttributes attrs = RequestContextHolder.getRequestAttributes();
-            return (String) attrs.getAttribute(TraceIdInterceptor.TRACE_ID, RequestAttributes.SCOPE_REQUEST);
-        } catch (Exception e) {
-            return null;
-        }
+        log.error("系统错误：", e);
+        return RetResult.fail(e.getCode(), e.getMessage());
     }
 }
