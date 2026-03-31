7c93074711484aaf5c55579edfbbac6b9a81ade1
WyattLau <wyatt6@163.com>
2026-03-31 22:39:47 +0800

    新增：追中上下文

---
+++ xianlai-common/src/main/java/fun/xianlai/common/context/TraceContext.java
@@ -0,0 +1,22 @@
+package fun.xianlai.common.context;
+
+/**
+ * @author WyattLau
+ */
+public final class TraceContext {
+    private static final ThreadLocal<String> TRACE_ID = new ThreadLocal<>();
+
+    public static void setTraceId(String traceId) {
+        TRACE_ID.set(traceId);
+    }
+
+    public static String getTraceId() {
+        return TRACE_ID.get();
+    }
+
+    public static void clear() {
+        TRACE_ID.remove();
+    }
+
+    private TraceContext() {}
+}
