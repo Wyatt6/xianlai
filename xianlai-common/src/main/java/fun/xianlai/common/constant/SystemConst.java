7745af2f7f9bc3b7384715d1fdf62530fae72ac4
WyattLau <wyatt6@163.com>
2026-03-31 11:31:42 +0800

    优化：系统缓存不需要区分frontLoad

--- xianlai-common/src/main/java/fun/xianlai/common/constant/SystemConst.java
+++ xianlai-common/src/main/java/fun/xianlai/common/constant/SystemConst.java
@@ -4,8 +4,7 @@ package fun.xianlai.common.constant;
  * @author WyattLau
  */
 public final class SystemConst {
-    public static final String CONFIG_ALL_CACHE_KEY = "system:config:all";
-    public static final String CONFIG_FRONT_LOAD_CACHE_KEY = "system:config:frontLoad";
+    public static final String CONFIG_CACHE_KEY = "system:config";
     public static final long DEFAULT_CACHE_HOURS = 6L;
 
     private SystemConst() {}
