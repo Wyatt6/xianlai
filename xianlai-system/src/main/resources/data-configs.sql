e212893e148a156885220baea1538ec7069f9e86
WyattLau <wyatt6@163.com>
2026-03-31 14:17:52 +0800

    Update data-configs.sql

--- xianlai-system/src/main/resources/data-configs.sql
+++ xianlai-system/src/main/resources/data-configs.sql
@@ -1,6 +1,6 @@
 INSERT IGNORE INTO tb_core_system_config(
-       `id`, `scope`, `enabled`, `front_load`, `name`, `config_key`, `config_value`, `value_type`, `remark`
+       `id`, `enabled`, `scope`, `front_load`, `name`, `config_key`, `config_value`, `value_type`, `remark`
 ) VALUES
-       (1, 'SYSTEM', 1, 0, '系统配置最后更新时间', 'system_config_update_time', current_timestamp, 'STRING', null);
+       (1, 1, 'SYSTEM', 0, '系统配置最后更新时间', 'system_config_update_time', current_timestamp, 'STRING', null);
 -- 复位非初始化数据的自增主键初值为100000
 ALTER TABLE tb_core_system_config AUTO_INCREMENT = 100000;
