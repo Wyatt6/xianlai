1926f42c7c42150d783ee732577f217c86725044
WyattLau <wyatt6@163.com>
2026-04-02 18:21:10 +0800

    优化：全局请求处理器日志打印和上下文缓存
    1. 区分外部请求和内部请求，日志打印格式不同；
    2. 增加请求处理结束后的日志打印；
    3. 优化从报文头获取数据并缓存到上下文

--- xianlai-common-starter/src/main/java/fun/xianlai/common/starter/filter/GlobalRequestFilter.java
+++ xianlai-common-starter/src/main/java/fun/xianlai/common/starter/filter/GlobalRequestFilter.java
@@ -2,6 +2,7 @@ package fun.xianlai.common.starter.filter;
 
 import fun.xianlai.common.constant.HeaderConst;
 import fun.xianlai.common.context.RequestContext;
+import fun.xianlai.common.utils.time.DateUtils;
 import jakarta.servlet.Filter;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
@@ -13,6 +14,7 @@ import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import java.io.IOException;
+import java.time.LocalDateTime;
 import java.util.UUID;
 
 /**
@@ -37,35 +39,74 @@ public class GlobalRequestFilter extends OncePerRequestFilter implements Filter 
 
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        boolean isHandle = false;
+        boolean isInner = false;
+        LocalDateTime enterTime = DateUtils.now();
         try {
             String path = request.getRequestURI();
-            if (shouldHandle(path)) {
-                // 1. 获取网关传递的traceId，并保存在上下文中
+            isHandle = shouldHandle(path);
+            if (isHandle) {
+                // traceId
                 String traceId = request.getHeader(HeaderConst.TRACE_ID);
-                if (traceId == null) {
+                if (traceId == null || traceId.isBlank()) {
                     traceId = UUID.randomUUID().toString().replaceAll("-", "").substring(0, 12);
+                    RequestContext.setTraceId(traceId);
+                    MDC.put("traceId", traceId);
+                    log.info("报文头 {} 不存在或为空，使用自生成的 traceId，不影响程序运行但链路跟踪可能出现异常", HeaderConst.TRACE_ID);
+                } else {
+                    RequestContext.setTraceId(traceId);
+                    MDC.put("traceId", traceId);
                 }
-                RequestContext.setTraceId(traceId);
-                MDC.put("traceId", traceId);
-                // 2. 获取网关传递的stTimestamp，并保存在上下文中
-                String stTimestampStr = request.getHeader(HeaderConst.ST_TIMESTAMP);
-                if (stTimestampStr != null && !stTimestampStr.isBlank()) {
-                    RequestContext.setStTimestamp(Long.parseLong(stTimestampStr));
+
+                // fromService
+                String fromService = request.getHeader(HeaderConst.FROM_SERVICE);
+                if (fromService != null && !fromService.isBlank()) {
+                    RequestContext.setFromService(fromService);
+                    if (!"gateway".equals(fromService)) {
+                        isInner = true;
+                    }
                 }
-                // 3. 获取网关传递的tenantId，并保存在上下文中
-                log.info("****** 请求: {} {}", request.getMethod(), request.getRequestURL());
+
+                log.info("==================== {} ====================", isInner ? "开始（内部）请求" : "开始请求");
+                log.info("Enter Time    : {}", DateUtils.timeMilliFormat(enterTime));
+                if (fromService == null || fromService.isBlank()) {
+                    log.info("From Service  : 报文头 {} 不存在或为空，不影响程序运行但无法确定上游服务", HeaderConst.FROM_SERVICE);
+                } else {
+                    log.info("From Service  : {}", fromService);
+                }
+                if (!isInner) {
+                    // beginTime
+                    String beginTimeStr = request.getHeader(HeaderConst.BEGIN_TIME);
+                    if (beginTimeStr != null && !beginTimeStr.isBlank()) {
+                        long beginTime = Long.parseLong(beginTimeStr);
+                        RequestContext.setBeginTime(beginTime);
+                        log.info("Begin Time    : {}", DateUtils.timeMilliFormat(DateUtils.milliTimstampToLocalDateTime(beginTime)));
+                    } else {
+                        log.info("Begin Time    : 报文头 {} 不存在，不影响程序运行但响应的 RetResult 对象的 beginTime 为默认值 0", HeaderConst.BEGIN_TIME);
+                    }
+                    // TODO 打印请求IP日志
+                    log.info("Request Method: {}", request.getMethod());
+                    log.info("Request URL   : {}", request.getRequestURL());
+                }
+                // TODO 打印脱敏token
+                // tenantId
                 String tenantIdStr = request.getHeader(HeaderConst.TENANT_ID);
                 if (tenantIdStr != null && !tenantIdStr.isBlank()) {
-                    Long tenantId = Long.parseLong(tenantIdStr);
-                    RequestContext.setTenantId(tenantId);
-                    log.info("tenantId: {}", tenantId);
+                    RequestContext.setTenantId(Long.parseLong(tenantIdStr));
+                    log.info("Tenant ID     : {}", tenantIdStr);
                 } else {
-                    log.info("报文头无tenantId");
+                    log.warn("Tenant ID     : 报文头 {} 不存在或为空，可能影响程序运行", tenantIdStr);
                 }
             }
             // 处理请求
             filterChain.doFilter(request, response);
         } finally {
+            if (isHandle) {
+                LocalDateTime exitTime = DateUtils.now();
+                log.info("Time Cost     : {}ms", DateUtils.betweenMillis(enterTime, exitTime));
+                log.info("Exit Time     : {}", DateUtils.timeMilliFormat(exitTime));
+                log.info("==================== {} ====================", isInner ? "完成（内部）请求" : "完成请求");
+            }
             MDC.clear();
             RequestContext.clear();
         }
--- xianlai-common/src/main/java/fun/xianlai/common/context/RequestContext.java
+++ xianlai-common/src/main/java/fun/xianlai/common/context/RequestContext.java
@@ -4,9 +4,18 @@ package fun.xianlai.common.context;
  * @author WyattLau
  */
 public final class RequestContext {
+    private static final ThreadLocal<String> FROM_SERVICE = new ThreadLocal<>();
     private static final ThreadLocal<Long> TENANT_ID = new ThreadLocal<>();
     private static final ThreadLocal<String> TRACE_ID = new ThreadLocal<>();
-    private static final ThreadLocal<Long> ST_TIMESTAMP = new ThreadLocal<>();
+    private static final ThreadLocal<Long> BEGIN_TIME = new ThreadLocal<>();        // 内部调用的时候不是必须的
+
+    public static void setFromService(String fromService) {
+        FROM_SERVICE.set(fromService);
+    }
+
+    public static String getFromService() {
+        return FROM_SERVICE.get();
+    }
 
     public static void setTenantId(Long tenantId) {
         TENANT_ID.set(tenantId);
@@ -24,18 +33,19 @@ public final class RequestContext {
         return TRACE_ID.get();
     }
 
-    public static void setStTimestamp(Long stTimestamp) {
-        ST_TIMESTAMP.set(stTimestamp);
+    public static void setBeginTime(Long beginTime) {
+        BEGIN_TIME.set(beginTime);
     }
 
-    public static Long getStTimestamp() {
-        return ST_TIMESTAMP.get();
+    public static Long getBeginTime() {
+        return BEGIN_TIME.get();
     }
 
     public static void clear() {
+        FROM_SERVICE.remove();
         TENANT_ID.remove();
         TRACE_ID.remove();
-        ST_TIMESTAMP.remove();
+        BEGIN_TIME.remove();
     }
 
     private RequestContext() {}
