0b126399e1fdf0b569446f6c95cb0b2763983e6b
WyattLau <wyatt6@163.com>
2026-04-09 15:43:19 +0800

    清除无效文件

--- xianlai-system-service/src/main/java/fun/xianlai/system/service/iam/model/form/ChangePasswordForm.java
+++
@@ -1,9 +0,0 @@
-package fun.xianlai.system.service.iam.model.form;
-
-import lombok.Data;
-
-@Data
-public class ChangePasswordForm {
-    private String oldPassword;     // 旧密码
-    private String newPassword;     // 新密码
-}
