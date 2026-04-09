1118f27a6c4c309fecb7d5c8aff26c11cb28cd2f
WyattLau <wyatt6@163.com>
2026-04-09 15:25:54 +0800

    优化：整合初始化SQL文件

--- xianlai-system-service/src/main/resources/data-tenant.sql
+++
@@ -1,8 +0,0 @@
-INSERT IGNORE INTO tb_core_tenant(
-       `id`, `code`, `domain`, `status`, `expire_time`, `logo`, `display_name`,
-       `contact_name`, `contact_gender`, `contact_phone`, `contact_email`
-) VALUES
-(1, 'default', null, 'NORMAL', null, null, '默认租户', null, 'UNKNOWN', null, null);
-
--- 复位非初始化数据的自增主键初值为100000
-ALTER TABLE tb_core_tenant AUTO_INCREMENT = 100000;
