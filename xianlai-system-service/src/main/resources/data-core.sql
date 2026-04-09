07ee19d122e221e681ab448634ebda690343acd7
WyattLau <wyatt6@163.com>
2026-04-09 15:30:11 +0800

    Update data-core.sql

--- xianlai-system-service/src/main/resources/data-core.sql
+++ xianlai-system-service/src/main/resources/data-core.sql
@@ -3,11 +3,12 @@
   租户
 =======
 */
-INSERT IGNORE INTO tb_core_tenant(
+INSERT
+IGNORE INTO tb_core_tenant(
        `id`, `code`, `domain`, `status`, `expire_time`, `logo`, `display_name`,
        `contact_name`, `contact_gender`, `contact_phone`, `contact_email`
 ) VALUES
-(1, 'default', null, 'NORMAL', null, null, '默认租户', null, 'UNKNOWN', null, null);
+    (1, 'default', null, 'NORMAL', null, null, '默认租户', null, 'UNKNOWN', null, null);
 -- 复位非初始化数据的自增主键初值为100000
 ALTER TABLE tb_core_tenant AUTO_INCREMENT = 100000;
 
