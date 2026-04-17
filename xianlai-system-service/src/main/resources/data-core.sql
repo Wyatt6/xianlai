599099bcf43df77e2da1a53fe56792882b0813a0
WyattLau <wyatt6@163.com>
2026-04-17 12:11:27 +0800

    修复：API数据的路径数值错误

--- xianlai-system-service/src/main/resources/data-core.sql
+++ xianlai-system-service/src/main/resources/data-core.sql
@@ -117,8 +117,8 @@ INSERT
 IGNORE INTO tb_core_api(
     `id`, `call_path`, `description`, `request_method`, `url`
 ) VALUES
-    (10001, 'core.init.getInitData', '获取初始化数据', 'GET', '/api/core/init/getInitData'),
-    (11001, 'core.captcha.getCaptcha', '获取验证码', 'GET', '/api/core/captcha/getCaptcha');
+    (10001, 'core.init.getInitData', '获取初始化数据', 'GET', '/api/system/core/init/getInitData'),
+    (11001, 'core.captcha.getCaptcha', '获取验证码', 'GET', '/api/system/core/captcha/getCaptcha');
 -- 复位非初始化数据的自增主键初值为100000
 ALTER TABLE tb_core_api AUTO_INCREMENT = 100000;
 
